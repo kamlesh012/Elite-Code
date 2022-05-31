f7e26b61e4f8b65c26e34f6e80740d7d7d0faa4d
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2022-05-31 22:15:22 +0530

    Time: 39 ms (9.70%), Space: 52 MB (6.57%) - LeetHub

---
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+     vector<vector<int>> sum(TreeNode *root,int target,int curr,vector<int> ans){
+         vector<int> temp;
+         vector<vector<int>>t1;
+        if(root && !root->left && !root->right)
+        {   
+            curr+=root->val;
+            ans.push_back(root->val);
+            if(curr==target)t1.push_back(ans);
+                return t1;
+        }
+        else if(root){
+            curr+=root->val;
+            ans.push_back(root->val);
+            vector<vector<int>> ok1=sum(root->left,target,curr,ans);
+            vector<vector<int>> ok2=sum(root->right,target,curr,ans);
+            if(!ok1.empty() && !ok1[0].empty()){
+                for(auto x:ok1){
+                    if(!x.empty())t1.push_back(x);
+                }
+            }
+            if(!ok2.empty() && !ok2[0].empty()){
+                for(auto x:ok2){
+                    if(!x.empty())t1.push_back(x);
+                }
+            }
+            return t1;
+        }
+        return t1;
+    }
+    
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> ans1;
+        vector<int> ans;
+        vector<vector<int>> a1=sum(root,targetSum,0,ans);
+        if(a1.empty())return ans1;
+        else return a1;
+    }
+};
