f2967c0f1fc23acb776a90e826922f9e5b2093bc
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2022-05-31 22:16:21 +0530

    Time: 54 ms (5.19%), Space: 51.9 MB (6.57%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -19,18 +19,22 @@ public:
             curr+=root->val;
             ans.push_back(root->val);
             if(curr==target)t1.push_back(ans);
+            
                 return t1;
         }
         else if(root){
             curr+=root->val;
             ans.push_back(root->val);
+            
             vector<vector<int>> ok1=sum(root->left,target,curr,ans);
             vector<vector<int>> ok2=sum(root->right,target,curr,ans);
+            
             if(!ok1.empty() && !ok1[0].empty()){
                 for(auto x:ok1){
                     if(!x.empty())t1.push_back(x);
                 }
             }
+            
             if(!ok2.empty() && !ok2[0].empty()){
                 for(auto x:ok2){
                     if(!x.empty())t1.push_back(x);
@@ -42,11 +46,7 @@ public:
     }
     
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        // vector<vector<int>> ans1;
         vector<int> ans;
-        // vector<vector<int>> a1=sum(root,targetSum,0,ans);
-        // if(a1.empty())return ans1;
-        // else return a1;
         return sum(root,targetSum,0,ans);
     }
 };
