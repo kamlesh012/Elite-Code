fdc4e34595e88353def2457e1021f1cff723dfe0
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2022-09-24 11:16:48 +0530

    Time: 37 ms (20.03%), Space: 39.1 MB (6.18%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -11,42 +11,21 @@
  */
 class Solution {
 public:
-     vector<vector<int>> sum(TreeNode *root,int target,int curr,vector<int> ans){
-         vector<int> temp;
-         vector<vector<int>>t1;
-        if(root && !root->left && !root->right)
-        {   
-            curr+=root->val;
+    vector<vector<int>> master;
+    void path(TreeNode *root,vector<int> ans,int targetSum){
+        if(root){
+            targetSum-=root->val;
             ans.push_back(root->val);
-            if(curr==target)t1.push_back(ans);
-            
-                return t1;
-        }
-        else if(root){
-            curr+=root->val;
-            ans.push_back(root->val);
-            
-            vector<vector<int>> ok1=sum(root->left,target,curr,ans);
-            vector<vector<int>> ok2=sum(root->right,target,curr,ans);
-            
-            if(!ok1.empty() && !ok1[0].empty()){
-                for(auto x:ok1){
-                    if(!x.empty())t1.push_back(x);
-                }
+            if(!root->left && !root->right && targetSum==0){
+                master.push_back(ans);
             }
-            
-            if(!ok2.empty() && !ok2[0].empty()){
-                for(auto x:ok2){
-                    if(!x.empty())t1.push_back(x);
-                }
-            }
-            return t1;
+            path(root->left,ans,targetSum);
+            path(root->right,ans,targetSum);
         }
-        return t1;
     }
-    
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<int> ans;
-        return sum(root,targetSum,0,ans);
+        vector<int> v;
+        path(root,v,targetSum);
+        return master;
     }
 };
