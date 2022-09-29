2a883c2e8acc395000262a3172acde6febe84a8f
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2022-09-29 13:57:38 +0530

    Time: 104 ms (53.88%), Space: 31.7 MB (75.84%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        //using two pointers
+        vector<int> ans;
+        int i=0,j=arr.size()-1;
+        while(j-i>=k){
+            if(abs(arr[i]-x)<=abs(arr[j]-x))j--;
+            else i++;
+        }
         
+        while(i<=j){
+            ans.push_back(arr[i]);
+            i++;
+        }
+        
+        return ans;
+        
+        //using minheap
+        /*
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         for(auto i:arr){
             pq.push({abs(x-i),i});
@@ -16,5 +32,6 @@ public:
         }
         sort(ans.begin(),ans.end());
         return ans;
+        */
     }
 };
