d80956b9392ca189ccb298315d9969da72c95175
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2022-10-11 14:43:55 +0530

    Time: 38 ms (5.47%), Space: 17.6 MB (5.95%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,50 +1,32 @@
 class Solution {
 public:
-     int f(int i,int buy,vector<int> &prices,vector<vector<int>>&dp,int n){
-        if(i==n) return 0;
-        if(dp[i][buy]!=-1) return dp[i][buy];
-        if(buy){
-            dp[i][buy]=max(-prices[i]+f(i+1,0,prices,dp,n),f(i+1,1,prices,dp,n));
-        }
-        else{
-            dp[i][buy]=max(+prices[i]+f(i+1,1,prices,dp,n),f(i+1,0,prices,dp,n));
+    int rec(vector<int> &a,int i,int n,int buy,vector<vector<int>>& dp){
+        if(i==n)return 0;
+        if(dp[i][buy]==-1)
+        {
+            if(buy){
+                    dp[i][buy]=max(-a[i]+rec(a,i+1,n,0,dp),rec(a,i+1,n,1,dp));
+            }
+            else{
+                    dp[i][buy]=max(a[i]+rec(a,i+1,n,1,dp),rec(a,i+1,n,0,dp));
+            }
         }
         return dp[i][buy];
     }
-    int maxProfit(vector<int>& prices) {
-        int l=prices.size();
-        vector<vector<int>>dp(l,vector<int>(2,-1));
-        return f(0,1,prices,dp,l);
-    }
-//     int rec(vector<int> &a,int i,int n,int buy,vector<vector<int>> dp){
-//         if(i==n)return 0;
-//         if(dp[i][buy]!=-1)return dp[i][buy];
-//         else
-//             // (dp[i][buy]==-1)
-//         {
-//             if(buy){
-//                     dp[i][buy]=max(-a[i]+rec(a,i+1,n,0,dp),rec(a,i+1,n,1,dp));
-//             }
-//             else{
-//                     dp[i][buy]=max(a[i]+rec(a,i+1,n,1,dp),rec(a,i+1,n,0,dp));
-//             }
-//             return dp[i][buy];
-//         }
-//     }
     
-//     int maxProfit(vector<int>& prices) {
-//         //DP
-//         int n=prices.size();
-//         vector<vector<int>> dp(n+1,vector<int>(2,-1));
-//         return rec(prices,0,n,1,dp);
+    int maxProfit(vector<int>& prices) {
+        //DP
+        int n=prices.size();
+        vector<vector<int>> dp(n+1,vector<int>(2,-1));
+        return rec(prices,0,n,1,dp);
         
-//         //Greedy
-//         // int ans=0;
-//         // for(int i=1;i<prices.size();i++){
-//         //     if(prices[i]>prices[i-1]){
-//         //         ans+=prices[i]-prices[i-1];
-//         //         }
-//         // }
-//         // return ans;
-//     }
+        //Greedy
+        // int ans=0;
+        // for(int i=1;i<prices.size();i++){
+        //     if(prices[i]>prices[i-1]){
+        //         ans+=prices[i]-prices[i-1];
+        //         }
+        // }
+        // return ans;
+    }
 };
