23e17add2352a5a6e783f0d95989eff13987e9ef
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2022-10-18 23:18:40 +0530

    Time: 185 ms (32.26%), Space: 27.5 MB (86.40%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,9 +1,42 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
-              //Multi-Source BFS.
+        //DP->NEW WAY LEARNT
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,INT_MAX));
+        //First Iterate for Top  & Left SIde
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==0)dp[i][j]=0;
+                else{
+                    int one=1e5,two=1e5;
+                    if(i-1>=0)one=dp[i-1][j]+1;
+                    if(j-1>=0)two=dp[i][j-1]+1;
+                    dp[i][j]=min({dp[i][j],one,two});
+                }
+            }
+        }
+        
+        //Then for Bottom & Right side from Bottom To Top
+        for(int i=n-1;i>=0;--i){
+            for(int j=m-1;j>=0;--j){
+                if(grid[i][j]){
+                    int one=1e5,two=1e5;
+                    if(i+1<n)one=dp[i+1][j]+1;
+                    if(j+1<m)two=dp[i][j+1]+1;
+                    dp[i][j]=min({dp[i][j],one,two});
+                }
+            }
+        }
+        //Why? Not NOrmal DP
+        //Because we don't know from which direction to traverse.
+        //so choosing all the directions & picking the minimum of all.
+        return dp;
+        
+        //Multi-Source BFS.
         //Shortest Paths.
         //Same as Rotten Tomatoes.
+            /*
         int n=grid.size(),m=grid[0].size();
         vector<vector<int>> visited(n,vector<int>(m,0)),distance(n,vector<int>(m,INT_MAX));
         queue<pair<int,int>> q;
@@ -65,8 +98,9 @@ public:
 //                 }
 //             }
 //         }
+//          return ans;
         
-        // return ans;
         return distance;
+        */
     }
 };
