ad871671af21cd89eb8ccddb4152f291ec3e65cb
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2022-10-31 22:07:52 +0530

    Time: 55 ms (28.05%), Space: 14.5 MB (31.53%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+         vector<vector<int>> adjl(n);
+        //converting to adjacency list
+        for(auto i:prerequisites){
+            adjl[i[1]].push_back(i[0]);
+        }
+        vector<int> indegree(n,0);
+        //calculating indegree of each node
+        for(auto i:adjl){
+            for(auto j:i){
+                indegree[j]++;
+            }
+        }
+        //kahn's algorithm for topological sorting.
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0)q.push(i);
+        }
+        
+        vector<int> ans;
+        while(!q.empty()){
+            int val=q.front();
+            q.pop();
+            ans.push_back(val); //storing the node that is to be visted first.
+            for(auto i:adjl[val]){
+                indegree[i]--;
+                if(indegree[i]==0)q.push(i);
+            }
+        }
+        if(ans.size()!=n)ans.clear();
+        return ans;
+    }
+};
