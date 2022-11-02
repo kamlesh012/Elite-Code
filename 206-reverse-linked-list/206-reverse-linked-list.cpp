f03efbe0cfa3e502efe3b7622978cd8b75e51573
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2022-11-03 00:36:59 +0530

    Time: 12 ms (56.28%), Space: 8.6 MB (9.57%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -10,18 +10,42 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        if(!head)return head;
-        ListNode *frwd=head->next;
-        ListNode *curr=head;
-        ListNode *prev=NULL;
-        while(curr){
-            curr->next=prev;
-            prev=curr;
-            curr=frwd;
-            if(frwd)
-                frwd=frwd->next;
+    
+    //Recursive Solution
+    ListNode *nhead=NULL;
+    ListNode *rec(ListNode *head){
+        if(!head || head->next==NULL){
+            //Keep the new head on the last node.
+            nhead=head;
+            return head;
         }
-        return prev;
+        ListNode * curr=rec(head->next);//move recursive call to next node
+        
+        curr->next=head;    //point the head of next node(returned from recursion) to current.
+        
+        head->next=NULL;       //set current's next to null.
+        
+        return head;
+    }
+    
+    ListNode* reverseList(ListNode* head) {
+        
+        if(!head)return head;
+        rec(head);
+        return nhead;
+        
+        //Iterative Solution
+        
+        // ListNode *frwd=head->next;
+        // ListNode *curr=head;
+        // ListNode *prev=NULL;
+        // while(curr){
+        //     curr->next=prev;
+        //     prev=curr;
+        //     curr=frwd;
+        //     if(frwd)
+        //         frwd=frwd->next;
+        // }
+        // return prev;
     }
 };
