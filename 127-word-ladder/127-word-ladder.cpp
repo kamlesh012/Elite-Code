02b16797faec16d7a2b6899845ef80d689daeaa5
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2022-11-23 08:46:31 +0530

    Time: 1443 ms (21.08%), Space: 32.1 MB (14.10%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,44 +1,46 @@
 class Solution {
 public:
+    //Striver's graph series 
     int ladderLength(string beg, string end, vector<string>& words) {
-        unordered_set<string> st;
-        
-        //inserting all into set for logn search & erase.
-        for(auto i:words){if(i!=beg)st.insert(i);}
-        
-        int n=beg.length();
-        
-        //creating a queue for different valid string & its level of occurence
-        queue<pair<string,int>> q;
-        
-        //insert the start string with level set to 1;
+        //set to store the words (for easy find & erase)
+        unordered_set<string> st(words.begin(),words.end());
+        //queue storing the word & shortest level at which it's generated/reached
+        queue<pair<string,int>>q;
+        //taking starting word to be at level 1
         q.push({beg,1});
-            
-        //applying bfs.
+        
+        //BFS
         while(!q.empty()){
-            string s=q.front().first;
-            int level=q.front().second;
             
-            if(s==end)return level; //if given string is endWord then return it's level
-            
+            pair<string,int> p=q.front();
+            string curr=p.first;
+            int level=p.second;
             q.pop();
-            st.erase(s);    //erasing the current string from set & queue to avoid cycle.
             
-            //for each word check all possible words that can be formed by changing a letter.
-            for(int i=0;i<s.length();i++){
-                string temp=s;
-                s[i]='a';
-                while(s[i]<='z'){
-                    //if the word formed by changing a letter is found in set
-                    if(st.find(s)!=st.end()){
-                        //push it to queue & increase it's level by 1.
-                        q.push({s,level+1});
+            //erase the current word as it has already been visited.
+            //need to remove so that other words don't visit it again.
+            //since we need shortest path.
+            st.erase(curr);
+            //if current word is end then return it's shortest level
+            if(curr==end)return level;
+            
+            //for each word try out all the different words that can be formed
+            int n=curr.size();
+            for(int i=0;i<n;i++){
+                string temp=curr;
+                char c='a';
+                while(c<='z'){
+                    temp[i]=c;
+                    //if the new word is found in set means it has not been visited
+                    //we will push it at next level into the queue
+                    if(st.find(temp)!=st.end()){
+                        q.push({temp,level+1});
                     }
-                    s[i]++;
+                    c++;
                 }
-                s=temp;
             }
         }
+        //if q is empty & we couldn't reach the end word that means no answer exists.
         
         return 0;
     }
