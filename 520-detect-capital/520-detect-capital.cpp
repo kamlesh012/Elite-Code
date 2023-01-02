aea7d7fd90451f7d45a1f3dc31315ee2c29c933b
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-01-02 11:12:16 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (70.23%) - LeetHub

--- 520-detect-capital/520-detect-capital.cpp
+++ 520-detect-capital/520-detect-capital.cpp
@@ -2,12 +2,7 @@ class Solution {
 public:
     bool detectCapitalUse(string word) {
         int cnt=0,ind=-1;
-        
-        for(int i=0;i<word.length();i++){
-            // (word[i]>=61 && word[i]<=91)?{cnt++;ind=i;}:{cnt=cnt;}
-            if(word[i]>=61 && word[i]<=91){cnt++;ind=i;}
-            else{cnt=cnt;}
-        }
+        for(int i=0;i<word.length();i++)(word[i]>=61 && word[i]<=91)?(cnt++,ind=i):(cnt=cnt);
         return (!cnt || (cnt==1 && ind==0) || cnt==word.length())?true:false;
     }
 };
