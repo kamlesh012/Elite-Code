d38b375a3c461006dcd81e6c5f911a873ba1738b
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-01-05 14:24:48 +0530

    Time: 516 ms (71.16%), Space: 89.7 MB (93.84%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //Comparator for sorting points in increasing order of first index.
     static bool comp(vector<int>&a,vector<int>&b){
         if(a[0]==b[0]){
             return a[1]<b[1];
@@ -8,10 +9,25 @@ public:
     }
     
     int findMinArrowShots(vector<vector<int>>& points) {
-        
         sort(points.begin(),points.end(),comp);
         int n=points.size();
         int cnt=1,curr=INT_MAX;
+        
+        //THe Idea is simple:-
+        //sort all points in increasing order of index.
+        //let each point be denoted as start & end (as given in problem satement).
+        
+        //Now while traversing the whole array
+        //at each index curr will represent the minimum end point(the max range till which we can burst the balloon with a single arrrow)
+        // in other words: curr=min(curr,end point of current point)
+        
+        //if curr is >= start of (i+1) :
+        //it means single arrow can shoot both ballons because intervals are overlapping so we will continue checking 
+        // if further intervals also overlap or not. & curr will still store the minimum of all endpoints in this overlap.
+        
+        //else if curr is <start of (i+1):
+        //it means the next balloon cannot be burst with the same arrow as previous ones because it's not overlapping.
+        //so we will incremeent the answer & do the same procedure from current index for further onwards.
         
         for(int i=0;i<n-1;i++){
             
