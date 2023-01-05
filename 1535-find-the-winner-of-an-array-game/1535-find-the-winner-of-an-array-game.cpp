72710fa837f3fe84363b565fc27e5ee010c8ae82
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-01-05 15:48:15 +0530

    Time: 237 ms (45.45%), Space: 63.3 MB (31.55%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
+        //Simple Observation :
+        //At a current instance in the game
+        //The first element that will have at least k smaller elements (consecutively) before or after it will be the winnner.
+        //if there is no such element than the max element will be the winner.
+        //as in next rounds no one can replace the max element.
+        
         int n=arr.size();
         int curr=arr[0];
         int cnt=0,mx=0;
+        
         for(int i=1;i<n;i++){
             if(arr[i]<curr)cnt++;
             else{
@@ -12,6 +19,7 @@ public:
             }
             if(cnt==k)return curr;
         }
+        
         return *max_element(arr.begin(),arr.end());
     }
 };
