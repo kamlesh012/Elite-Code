b04bdbbcf228388193d41a7f0504af66607468d1
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-03-03 18:23:42 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (62.84%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //Predicate
     bool predicate(vector<int> &a,int k,int mx){
         int splits=1,sum=0;
         for(int i=0;i<a.size();i++){
@@ -14,28 +15,22 @@ public:
     
     int splitArray(vector<int>& wt, int k) {
         int n=wt.size();
+        
+        //Lower-Boundary (Not Inclusive)
         int l=*max_element(wt.begin(),wt.end())-1;
+        
+        //Upper-Boundary (Not Inclusive)
         int h=accumulate(wt.begin(),wt.end(),0ll)+1;
+        
         int ans=-1;
-        // while(l<=h){
-        //     int mid=((h-l)/2)+l;
-        //     if(predicate(wt,k,mid)){
-        //         ans=mid;
-        //         h=mid-1;
-        //     }
-        //     else l=mid+1;
-        // }
+        
+        //Invariant Binary Search
         while(h-l>1){
-            
             int mid=((h-l)/2)+l;
-            
-            if(predicate(wt,k,mid)){
-                // ans=mid;
-                h=mid;
-            }
+            if(predicate(wt,k,mid))h=mid;            
             else l=mid;
         }
+        
         return h;
-        // return ans;
     }
 };
