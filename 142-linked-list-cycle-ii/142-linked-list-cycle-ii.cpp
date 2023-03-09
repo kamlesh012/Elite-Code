2a30df1d3656993f91dd6170b2bcdd82c264baa2
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-03-09 17:27:29 +0530

    Time: 16 ms (23.25%), Space: 9.3 MB (23.63%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -14,20 +14,36 @@ public:
     //If we travel from head & m in forward direction then they will meet at the point where cycle begins in a Linked List.
     
     ListNode *detectCycle(ListNode *head) {
-        ListNode *start=head,*slow=head,*fast=head;
-        while(fast && fast->next){
-            fast=fast->next;
-            slow=slow->next;
-            if(fast)fast=fast->next;
-            
-            if(slow==fast){
-                while(start!=fast){
-                    start=start->next;
-                    fast=fast->next;
-                }
-                return start;
+        
+        //O(N) Time & Space Complexity
+        unordered_set<ListNode*> s;
+        while(head){
+            if(s.find(head)!=s.end()){
+                return head;
             }
+            else{
+                s.insert(head);
+            }
+            head=head->next;
         }
         return NULL;
+        
+        //O(N) Time & O(1) Space Complexity
+        
+//         ListNode *start=head,*slow=head,*fast=head;
+//         while(fast && fast->next){
+//             fast=fast->next;
+//             slow=slow->next;
+//             if(fast)fast=fast->next;
+            
+//             if(slow==fast){
+//                 while(start!=fast){
+//                     start=start->next;
+//                     fast=fast->next;
+//                 }
+//                 return start;
+//             }
+//         }
+//         return NULL;
     }
 };
