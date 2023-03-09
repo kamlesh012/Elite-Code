8e0f26a09df5f0cbb4f19912feed08643f022704
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-03-09 12:12:19 +0530

    Time: 178 ms (82.68%), Space: 66.6 MB (84.49%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -4,19 +4,28 @@ public:
     //Predicate Function
     bool predicate(vector<int>&b,int m,int k,int days){
         int mx=INT_MIN;
-        int splits=0,n=b.size();
+        int splits=0,n=b.size(),cnt=0;
         //splits will store the count no. of subarrays of length k in which the max value is smaller than or equal to days.
-        for(int i=0;i<=n-k;){
-            bool ok=false;
-            int j=i;
-            for(;j<n && j-i<k;j++){
-                if(b[j]>days)break;
-            }
-            if(j-i==k){
-                i=j;
+        for(int i=0;i<n;i++){
+            
+            //Leetcode's Easy Way
+            if(b[i]<=days)cnt++;
+            else cnt=0;
+            if(cnt==k){
                 splits++;
+                cnt=0;
             }
-            else i=j+1;
+            
+            //My Hard Way
+            // int j=i;
+            // for(;j<n && j-i<k;j++){
+            //     if(b[j]>days)break;
+            // }
+            // if(j-i==k){
+            //     i=j;
+            //     splits++;
+            // }
+            // else i=j+1;
         }
         return splits>=m;
     }
