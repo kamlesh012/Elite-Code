d61664095b454ebb131b4df3d0b15ee736e2c284
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-03-19 23:49:36 +0530

    Time: 1564 ms (50.29%), Space: 566.1 MB (43.42%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -1,3 +1,71 @@
+struct Node{
+    Node* links[26]={NULL};
+    bool flag = false;
+    bool containKey(char ch)
+    {
+        return (links[ch-'a']!=NULL);
+    }
+    void put(char ch, Node* node)
+    {
+        links[ch -'a'] = node;
+    }
+    Node* get(char ch)
+    {
+        return links[ch-'a'];
+    }
+    void setEnd(){
+        flag = true;
+    }
+    bool isEnd(){
+        return flag;
+    }
+};
+
+class WordDictionary {
+private:
+    Node* root;
+public:
+    WordDictionary() {
+        root= new Node();
+    }
+    
+    void addWord(string word) {
+        Node* node = root;
+        for(int i = 0;i<word.size();i++)
+        {
+            if(!(node->containKey(word[i])))
+            {
+                node->links[word[i]-'a']=new Node();
+                // node->put(word[i],new Node());
+            }
+            node= node->get(word[i]);
+        }
+        node->setEnd();
+    }
+
+     bool searchHelper(string word, Node* node){
+        for(int i=0; i<word.size(); i++){
+            if(word[i] == '.'){
+                for(char c='a';c<='z';c++){
+                    if(node->containKey(c)){
+                        if(searchHelper(word.substr(i+1), node->get(c)))
+                            return true;
+                    }
+                }
+                return false;
+            }
+            else if(!node->containKey(word[i])) return false;
+            node = node->get(word[i]);
+        }
+        return node->isEnd();
+    }
+    
+    bool search(string word) {
+        Node* node = root;
+        return searchHelper(word,node);
+    }
+};
+/*
 class Trie {
     Trie* children[26]={NULL};
     bool isEnd=false;
@@ -48,6 +116,7 @@ public:
         return node->search(word,node);
     }
 };
+*/
 
 /**
  * Your WordDictionary object will be instantiated and called as such:
