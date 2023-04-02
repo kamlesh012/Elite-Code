6ea242e34f98cdc772b922a09983c94999c62e3d
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-04-02 07:51:14 +0530

    Time: 65 ms (47.27%), Space: 41.2 MB (33.45%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+        //O(N) solution.
+        //Intuition is to place each element at its correct position (i-th)index by swapping 
+        //it with the element that is at its position repeatedly until max elements are at their
+        //respective positions(i-th).
+        //not considering duplicates,non positives,greater than n numbers.
+        
         int n=nums.size();
         for(int i=0;i<n;i++){
+            //Not considering non positives,numbers greater than n,elements already at their place,elements at whose place the correct element doesn't already lie.
+            
+            //consider the case [1,1] for last case.
             while(nums[i]>0 && nums[i]<=n && nums[i]!=i+1 && nums[nums[i]-1]!=nums[i]){
                 swap(nums[i],nums[nums[i]-1]);
             }
