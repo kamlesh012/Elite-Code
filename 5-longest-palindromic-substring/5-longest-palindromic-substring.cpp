a161e91262291f44e8eac83a7a0f470da6f8b0ad
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-04-04 23:30:51 +0530

    Time: 16 ms (78.73%), Space: 7 MB (76.22%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    // bool ispal(string &s,int )
+    string longestPalindrome(string s) {
+        int n=s.length(),mx=0,cnt=0;
+        string ans;
+        for(int i=0;i<n;i++){
+            int l=i-1,r=i+1,curr=1;
+            if(s[i]==s[i+1])
+            {
+                int rr=r+1,currr=curr+1,ll=l;
+                while(ll>=0 && rr<=n && s[ll]==s[rr]){
+                    ll--;
+                    rr++;
+                    currr+=2;
+                }
+                if(currr>mx){
+                    ans.clear();
+                    for(int x=ll+1;x<rr;x++){
+                        ans.push_back(s[x]);
+                    }
+                    mx=currr;
+                }
+                
+            }
+            while(l>=0 && r<=n && s[l]==s[r]){
+                l--;
+                r++;
+                curr+=2;
+            }
+            if(curr>mx){
+                ans.clear();
+                for(int x=l+1;x<r;x++){
+                    ans.push_back(s[x]);
+                }
+                mx=curr;
+            }
+        }
+        return ans;
+    }
+};
