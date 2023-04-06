d049c60c878068c6406509e459e6e7e2012ac2f6
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-04-06 13:48:48 +0530

    Time: 429 ms (5.10%), Space: 100.6 MB (6.34%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        multiset<int> mst(nums.begin(),nums.end());
+        int ans=0;
+        sort(nums.begin(),nums.end());
+        for(auto i:nums){
+            auto it=mst.find(i);
+            if(it!=mst.end()){
+                mst.erase(it);
+                auto req=mst.find(k-i);
+                if(req!=mst.end()){
+                    mst.erase(req);
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
