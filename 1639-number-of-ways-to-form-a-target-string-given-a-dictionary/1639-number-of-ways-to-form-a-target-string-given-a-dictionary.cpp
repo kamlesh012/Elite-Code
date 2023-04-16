3cce89dc429ec1a5a81d1bf55aa89d367b1bb505
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-04-17 00:28:37 +0530

    Time: 320 ms (52.16%), Space: 86.1 MB (8.62%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,6 +1,8 @@
 #define mod 1000000007
 class Solution {
 public:
+    /*
+    //Memoization
     vector<vector<int>> dp;
     long long rec(int i,int j,vector<vector<int>> &freq,vector<string> &words,string &target){
         if(j==target.size())return 1;
@@ -17,18 +19,38 @@ public:
         }
         return dp[i][j];
     }
+    */
     
     int numWays(vector<string>& words, string target) {
         int n=words[0].size();
         vector<vector<int>> freq(n,vector<int>(26,0));
-        dp.resize(words[0].size(),vector<int>(target.size(),-1));
         for(auto word:words){
             for(int j=0;j<word.size();j++){
-                // cout<<j<<" "<<word[j]<<endl;
                 freq[j][word[j]-'a']++;
-                // cout<<freq[j][word[j]-'a']<<endl;
             }
         }
-        return rec(0,0,freq,words,target);
+        
+        //Memoization:
+          // dp.resize(words[0].size(),vector<int>(target.size(),-1));
+          // return rec(0,0,freq,words,target);
+        
+        
+        //Tabulation 
+        vector<vector<long long>>dp(words[0].size()+1,vector<long long>(target.size()+1,0ll));
+        
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=target.size();j++){
+             if(j==0)dp[i][j]=1;
+                else if(i==0)dp[i][j]=0;
+                else{
+                    long long pick=0ll,notpick=0ll;
+                    pick=((freq[i-1][target[j-1]-'a']%mod)*(dp[i-1][j-1]%mod))%mod;
+                    notpick=dp[i-1][j];
+                    dp[i][j]=(pick+notpick)%mod;
+                }
+            }
+        }
+        return dp[n][target.size()];
+      
     }
 };
