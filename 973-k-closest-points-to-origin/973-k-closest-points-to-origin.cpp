273b2756ed19f926d656955cd3927d270f391777
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-04-17 20:17:12 +0530

    Time: 250 ms (56.65%), Space: 73.3 MB (36.50%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -3,20 +3,23 @@ public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         
         priority_queue<pair
-            <int,pair<int,int>>,
-        vector<pair<int,pair<int,int>>>
-            ,greater<pair<int,pair<int,int>>>
+            <double,pair<int,int>>,
+        vector<pair<double,pair<int,int>>>
+            ,greater<pair<double,pair<int,int>>>
             > pq;
         
         for(auto i:points){
-            //Basci Observation that I missed.
-            //Not using sqrt will yeild the same result
-            int distance=(((i[0]*i[0])+(i[1]*i[1])));
+            //Basic Observation that I missed.
+            //If using sqrt use double as datatype
+            
+            //else the same problem can also be solved without using sqrt at all.
+            
+            double number=((i[0]*i[0])+(i[1]*i[1]));
+            double distance=sqrt(number);
             pq.push({distance,{i[0],i[1]}});
         }
         vector<vector<int>> ans;
         for(int i=0;i<k;i++){
-            // cout<<pq.top().first<<" "<<pq.top().second.first<<" "<<pq.top().second.second<<endl;
             ans.push_back({pq.top().second.first,pq.top().second.second});
             pq.pop();
         }
