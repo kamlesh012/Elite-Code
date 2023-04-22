13d910d45690e90783a2c00f79936bc1ab6bace4
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-04-22 19:53:43 +0530

    Time: 15 ms (20.84%), Space: 17.4 MB (65.22%) - LeetHub

---
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -0,0 +1,81 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        queue<pair<TreeNode*,int>> q;
+        int cnt=0,ans=0;
+        q.push({root,cnt});
+        // int level=0;
+        while(q.size()){
+            int n=q.size();
+            int flag=0,first=-1,last=0;
+            // level++;
+                // cout<<level<<endl;
+            //a way to avoid cnt(values of nodes) from getting large.
+            //explained below: or check Aryan Mittal's Video/Discuss solution.
+            
+            int reduce_node_value=q.front().second;
+            for(int i=0;i<n;i++){
+                auto it=q.front();
+                q.pop();
+                
+                // int nodes_at_curr_level=1<<level;
+                //Technically this is also correct,to take the max nodes possible till this previous level & subtract current nodes value(cnt) to avoid values from getting larget but  the total no. of nodes till previous level is overflowing itself,as 1<<1500 can get out of bounds.
+                //Note that: if a same value is subtracted from all nodes then their difference will still remain the same. 
+                //So we we are just subtracting the number of nodes at just previos level
+                //each time so that the values won't get bigger.
+                // cout<<nodes_at_curr_level<<endl;
+                
+                //so to avoid overflow we are using the above method.
+                if(first==-1)first=it.second;
+                last=it.second;
+                int dist=it.second-reduce_node_value;
+                if(it.first->left){
+                        q.push({it.first->left,(2ll*dist+1)});
+                }
+                if(it.first->right){
+                    q.push({it.first->right,(2ll*dist+2)});
+                } 
+            }
+            ans=max(ans,last-first+1);
+        }
+        return ans;
+        
+        //TLEd Simulation:
+        // while(q.size()){
+        //     int n=q.size();
+        //     int flag=0,first=-1,last=0;
+        //     for(int i=0;i<n;i++){
+        //         auto it=q.front();
+        //         q.pop();
+        //         if(it.first==nullptr){
+        //             q.push({it.first,cnt+1});
+        //             cnt++;
+        //             q.push({it.first,cnt+1});
+        //             cnt++;
+        //         }
+        //         else{
+        //             if(first==-1)first=it.second;
+        //             last=it.second;
+        //             q.push({it.first->left,cnt+1});
+        //             cnt++;
+        //             q.push({it.first->right,cnt+1});
+        //             cnt++;
+        //         }     
+        //     }
+        //     if(first==-1)break;
+        //     ans=max(ans,last-first+1);
+        // }
+        // return ans;
+    }
+};
