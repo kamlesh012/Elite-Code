f20104c7218d192e1ff99cdab9bd16e23ae8c5c1
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-04-27 20:29:54 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (94.29%) - LeetHub

--- 319-bulb-switcher/319-bulb-switcher.cpp
+++ 319-bulb-switcher/319-bulb-switcher.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
+        /*My code
         int ans=0;
         while((ans*ans)<=n){
             ans++;
         }
         return ans-1;
+        */
+        //Editorialist's COde
+        return sqrt(n);
     }
 };
