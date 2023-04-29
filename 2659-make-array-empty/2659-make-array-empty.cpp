04e02c8e69444467293795759b52a205e427df84
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-04-29 22:01:34 +0530

    Time: 422 ms (76.92%), Space: 110.7 MB (53.85%) - LeetHub

---
+++ 2659-make-array-empty/2659-make-array-empty.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    long long countOperationsToEmptyArray(vector<int>& nums) {
+        map<int,int> mp;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            mp[nums[i]]=i;
+        }
+        // int mx=*max_element(nums.begin(),nums.end());
+        long long int ans=n,prev=0ll,taken=0ll,currtaken=0;
+        for(auto i:mp){
+            // if(i.first==mx){
+            //     ans+=(n-taken);
+            // }
+            // else 
+                if(i.second<prev){
+                ans+=n-(taken);
+                currtaken=taken;
+                taken++;
+            }
+            else taken++;
+            // if(prev==0){
+            //     ans+=i.second;
+            //     taken++;
+            // }
+            // else if(i.second>prev){
+            //     // ans+=i.second-prev-currtaken;
+            //     ans+=i.second-prev;
+            //     taken++;
+            // }
+            // else{
+            //     currtaken=taken;
+            //     ans+=i.second-taken;
+            //     taken++;
+            // }
+            prev=i.second;
+            
+        }
+        if(ans==0)ans=n;
+        return ans;
+    }
+};
