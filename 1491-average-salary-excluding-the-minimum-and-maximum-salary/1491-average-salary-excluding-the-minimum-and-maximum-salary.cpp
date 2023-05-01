52523d2378f10d28b93d26423b5d7e84509948e9
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-05-01 08:01:58 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (84.06%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double average(vector<int>& sal) {
+       sort(sal.begin(),sal.end());
+        int n=sal.size();
+        double sum=0;
+        
+        for(int i=1;i<n-1;i++){
+            sum+=sal[i];
+        }
+        return sum/((n-2)*1.0);
+        
+    }
+};
