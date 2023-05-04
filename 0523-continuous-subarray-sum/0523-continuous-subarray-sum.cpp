348c5d46c108e4a828e7fe5cda90ba154d7db8b6
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-05-04 23:16:40 +0530

    Time: 266 ms (20.56%), Space: 113.7 MB (75.61%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -5,16 +5,36 @@ public:
         for(int i=1;i<n;i++){
             nums[i]+=nums[i-1];
         }
+        
         unordered_map<int,int> mp;
+        //hashmap<sum,index>
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]%k==0)return true;
+            else if(mp.count(nums[i]%k)==0){
+                mp[nums[i]%k]=i;
+            }
+            else if(mp[nums[i]%k]<i-1)return true;
+        }
+        return false;
+        /*
+//         MY Approach: check %k till current index but don't add previos index.
+
         int prev=nums[0]%k;
         for(int i=1;i<n;i++){
+        //in case if current element is divisible return true.
+        //since i>0 size is already >0.
             if(nums[i]%k==0)return true;
+            
             if(mp[nums[i]%k]){
                 return true;
             }
+            //now add previous to answer.
             mp[prev]++;
+            
+            //the current one become previous now.
             prev=nums[i]%k;
         }
         return false;
+        */
     }
 };
