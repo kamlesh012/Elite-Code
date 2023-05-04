8a6e783dc7934e8c41da287a9649880662e3e5f0
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-05-05 00:18:32 +0530

    Time: 60 ms (57.52%), Space: 31.9 MB (19.40%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int n=nums.size();
+        map<int,int> mp;
+        int sum=0;
+        //I was doing mistake in Modulo Arithmetic.
+        
+        int ans=0;
+        mp[0]=1;
+        for(int i=0;i<n;i++){
+            sum+=(nums[i]);
+            //Need to do +k to calculate remainders of negative numbers.
+            ans+=mp[(sum%k+k)%k]++;
+            // sum=sum%k;
+            // mp[(sum%k+k)%k]++;
+        }
+        
+   //Bad Implementation     
+        // for(auto i:mp){
+            // // if(i.first==0){
+            //     int cnt=i.second;
+            //     ans+=(cnt*(cnt+1))/2;
+            // // }
+            // else if(i.second>1){
+            // int cnt=i.second;
+            // ans+=(cnt*(cnt-1))/2;
+            // }
+        // }
+        return ans;
+    }
+};
