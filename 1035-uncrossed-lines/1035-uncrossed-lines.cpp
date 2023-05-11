a9767db16e3109a76c5755c54999b989507d3092
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-05-12 00:57:34 +0530

    Time: 33 ms (24.24%), Space: 13 MB (63.96%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -3,7 +3,7 @@ public:
     //LOL What a Fluke!
     int rec(vector<int> &a,vector<int>&b,int i,int j,vector<vector<int>> &dp){
         int n=a.size(),m=b.size();
-        if(i>=n || j>=m)return 0;
+        if(i==n || j==m)return 0;
         if(dp[i][j]==-1){
             if(a[i]==b[j])return dp[i][j]=rec(a,b,i+1,j+1,dp)+1;
             return dp[i][j]=max(rec(a,b,i+1,j,dp),rec(a,b,i,j+1,dp));
@@ -14,6 +14,7 @@ public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
         int n=nums1.size(),m=nums2.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        //Tabulation Pending.
         return rec(nums1,nums2,0,0,dp);
     }
 };
