894d5ed5c487bec5de073f38e922f7ce9c1059ca
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-05-13 00:06:59 +0530

    Time: 410 ms (15.28%), Space: 123 MB (34.83%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,6 +1,6 @@
 class Solution{
 public:
-    long long rec(vector<vector<int>> &q,int i,vector<int> &dp){
+    long long rec(vector<vector<int>> &q,int i,vector<long long> &dp){
         if(i>=q.size())return 0ll;
         if(dp[i]==-1){
             long long pick=0ll,notpick=0ll;
@@ -16,16 +16,16 @@ public:
         int n=questions.size();
         
         //Memoized
-        // vector<int> dp(n+1,-1);
-        // return rec(questions,0,dp);
+        vector<long long> dp(n+1,-1ll);
+        return rec(questions,0,dp);
         
         //Tabulated
-        vector<long long> dp(n,0ll);
-        for(int i=n-1;i>=0;--i){
-            long long points=0ll;
-            if(i+questions[i][1]+1<n)points=dp[i+questions[i][1]+1];
-            dp[i]=max(questions[i][0]+points,i+1<n?dp[i+1]:0ll);
-        }
-        return dp[0];   
+        // vector<long long> dp(n,0ll);
+        // for(int i=n-1;i>=0;--i){
+        //     long long points=0ll;
+        //     if(i+questions[i][1]+1<n)points=dp[i+questions[i][1]+1];
+        //     dp[i]=max(questions[i][0]+points,i+1<n?dp[i+1]:0ll);
+        // }
+        // return dp[0];   
     }
 };
