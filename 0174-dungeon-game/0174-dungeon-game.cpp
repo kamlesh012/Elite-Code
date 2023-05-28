3d4ad905517a375d8b814c1d53809be51a53b448
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-05-28 13:44:55 +0530

    Time: 3 ms (97.16%), Space: 8.7 MB (91.00%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.cpp
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -2,16 +2,19 @@ class Solution {
 public:
     int calculateMinimumHP(vector<vector<int>>& a) {
         int n=a.size(),m=a[0].size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        int dp[201][201];
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         for(int i=n-1;i>=0;--i){
             for(int j=m-1;j>=0;--j){
                 if(i==n-1 && j==m-1){
+                    //Base Case
                     dp[i][j]=(a[i][j]>=0)?1:-a[i][j]+1;
                 }
                 else{
                     int bottom=1e9,right=1e9;
                     if(i+1<n)bottom=dp[i+1][j];
                     if(j+1<m)right=dp[i][j+1];
+                    
                     int ans=min(bottom,right)-a[i][j];
                     if(ans<=0)ans=1;
                     dp[i][j]=ans;
