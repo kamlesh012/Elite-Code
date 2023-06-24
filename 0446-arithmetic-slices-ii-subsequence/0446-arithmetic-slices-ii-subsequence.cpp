24f498a6979bc100a6b701845cd159aa1e06b41e
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-06-24 12:10:58 +0530

    Time: 1655 ms (14.31%), Space: 285.8 MB (7.69%) - LeetHub

---
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+       //Read Official Editorial for Clarificatiosn.
+        //Dry Run on Both Sample TCs more than enough to get the idea.What I am doing.
+    int numberOfArithmeticSlices(vector<int>& nums) {
+      int n=nums.size(),ans=0;
+        unordered_map<long long,unordered_map<long long,long long>> dp;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                
+                long long diff=(long long)nums[i]-(long long)nums[j];
+                
+                //Stores count of all arithmetic subsequnces(length at least 2)
+                dp[i][diff]+=dp[j][diff]+1;
+                
+                //but we need subsequences of length>2. so  will not include
+                //the newest subsequence formed{nums[j],nums[i]} as it is of length 2;
+                //but it will be stored in dp as subsequences of length 2 are counted for future use.
+                ans+=dp[j][diff];
+                
+            }
+        }
+        return ans;
+        
+    }
+};
