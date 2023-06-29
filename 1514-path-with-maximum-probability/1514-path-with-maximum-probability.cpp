0f7e8142b825c9f817956a64239de841269749d5
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-06-29 08:13:42 +0530

    Time: 451 ms (7.18%), Space: 67.3 MB (37.79%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -7,13 +7,21 @@ public:
             adjl[edges[i][0]].push_back({edges[i][1],weight[i]*(-1)});
             adjl[edges[i][1]].push_back({edges[i][0],weight[i]*(-1)});
         }
+        //how is this code working for three connected nodes with weights
+        //0.5 0.3 && 0.5
+        ///dry run.
+        
+        vector<double> dist(n+1,1);//setting as 1 because 1 is the max dist possible. b/w 0 & 1.you can take it as infinity also.
         
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        pq.push({0,start});
-        vector<double> dist(n+1,1e9);
+        //Pushing 1 to priorityqueue as (-1) is the minimum distance possible b/w -1 & 0.
+        //since start node will have min distance with itself,
+        //so dist[start]=-1;     will be considering 1 as (-1) down below.
+        pq.push({1,start});
         dist[start]=1;
         
         while(pq.size()){
+            
             auto it=pq.top();
             pq.pop();
             
@@ -21,16 +29,18 @@ public:
             int curr_wt=it.first;
             
             for(auto child:adjl[curr]){
+                
                 double wt=abs(child.second);
                 int node=child.first;
                 
-                if(dist[node]>((-1)*(abs(dist[curr])*wt))){
-                    dist[node]=((-1)*(abs(dist[curr])*wt));
+                if(dist[node]>(-1*abs(dist[curr])*wt)){
+                    dist[node]=(-1*abs(dist[curr])*wt);
                     pq.push({dist[node],node});
                 }
+                
             }
-        }
-        if(dist[end]==1e9)return 0;
+       } 
+        if(dist[end]==1)return 0;
         return -1*dist[end];
     }
 };
