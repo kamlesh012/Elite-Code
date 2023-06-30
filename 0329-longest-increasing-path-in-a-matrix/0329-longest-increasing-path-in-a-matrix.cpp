f07a8f30f5458250efe69c9ef149b0cd7ccaa377
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-06-30 08:52:33 +0530

    Time: 52 ms (63.93%), Space: 16.7 MB (29.71%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int n=matrix.size(),m=matrix[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        int ans=0;
+        function<int(int,int)> dfs=[&](int i,int j){
+            int res=0;
+            for(int x=-1;x<=1;x++){
+                for(int y=-1;y<=1;y++){
+                    if((!x && y) || (x && !y)){
+                        if(i+x>=0 && j+y>=0 && i+x<n && j+y<m && matrix[i+x][j+y]>matrix[i][j]){
+                            if(vis[i+x][j+y])res=max(res,vis[i+x][j+y]);
+                            else res=max(res,dfs(i+x,j+y));
+                        }
+                            
+                    }
+                }
+            }
+            vis[i][j]=res+1;
+            return res+1;
+        };
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j]){
+                    ans=max(ans, dfs(i,j));
+                }
+            }
+        }
+        return ans;
+    }
+};
