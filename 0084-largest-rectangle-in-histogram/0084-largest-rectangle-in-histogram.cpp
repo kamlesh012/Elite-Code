40932351d4dea0faa1c10f1ae0cbda1cb05b5ab3
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-07-06 10:13:51 +0530

    Time: 234 ms (40.75%), Space: 81.2 MB (61.73%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
 
     int largestRectangleArea(vector<int>& a) {
-        //a lot of doubts still
-  int n=a.size();
+    int n=a.size();
     vector<int> nge(n,0),pge(n,0);
     stack<int> stk;
     //next smaller element
     for(int i=n-1;i>=0;--i){
         while(!stk.empty() && a[stk.top()]>=a[i])stk.pop();
-        if(stk.empty())nge[i]=n;
+        if(stk.empty())nge[i]=n;// n denotes invalid index.greater than range.
         else nge[i]=stk.top();
         stk.push(i);
     }
     
     while(!stk.empty())stk.pop();
+        
     //previous smaller element
     for(int i=0;i<n;i++){
         while(!stk.empty() && a[stk.top()]>=a[i])stk.pop();
-        if(stk.empty())pge[i]=-1;
+        if(stk.empty())pge[i]=-1;//invalid index
         else pge[i]=stk.top();
         stk.push(i);
     }
@@ -26,11 +26,9 @@ public:
     //difference between nge & pge * length = area.
     for(int i=0;i<n;i++){
         
-        // if(pge[i]==-1)pge[i]=-1;        //don't know how this is working
-        // if(nge[i]==-1)nge[i]=n;     //saw this is discss section.don't know why not same for pge.
-        //working how? still to debug.
+        // if(pge[i]==-1)pge[i]=-1;        
+        // if(nge[i]==-1)nge[i]=n;    
         
-        // cout<<left<<" "<<right<< " "<<a[i]<<endl;
         ans=max(ans,(nge[i]-(pge[i]+1))*a[i]);
     }
     return ans;
