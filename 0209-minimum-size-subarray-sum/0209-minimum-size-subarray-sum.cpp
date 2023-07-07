f1a33c25c729234b94703b90f8920990371517c2
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-07-07 10:22:08 +0530

    Time: 38 ms (82.96%), Space: 28.1 MB (71.41%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        
+        //Variable Size Sliding WIndow.
         int i=0,j=0,n=nums.size(),sum=0,len=INT_MAX;
         
         while(j<n){
             sum+=nums[j];
             while(sum>=target){
                 len=min(len,j-i+1);
-                // cout<<j<<" "<<i<<" length "<<len<<endl;
-                // cout<<"INside sum "<<sum<<endl;
                 sum-=nums[i];
                 i++;
             }
             j++;
-            // cout<<"SUM "<<sum<<endl;
-            // cout<<"i "<<i<<" j "<<j<<endl;
         }
         
         return (len==INT_MAX)?0:len;
