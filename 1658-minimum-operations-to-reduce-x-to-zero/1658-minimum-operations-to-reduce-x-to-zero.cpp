d329c53af249304c4d0f56b9ef36a2a073bfa6d0
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-07-07 12:54:24 +0530

    Time: 176 ms (88.67%), Space: 98.4 MB (75.66%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        // nums.push_back(0);
         int l=0,n=nums.size(),sum=0;
+        
+        //Add element from right until sum>x
         int r=n-1;
         while(r>=0 && sum<=x){
             sum+=nums[r];
             r--;
         }
+        
+        //r should point on the first element from the right that is added to sum
         r++;
         
-        // cout<<sum<<endl;
-        // cout<<l<<" start "<<r<<endl;
         int len=INT_MAX;
-        // if(sum==)
+        
+        //both conditions are equal to n as a case where l will cover the whole array is taken into consideration.
+        
         while(l<=r && r<=n){
-            // while(r<n && sum>x){
-            //     sum-=nums[r];
-            //     r++;
-            // }
-            // cout<<"After first sum: "<<sum<<" l "<<l<<" r "<<r<<endl;
+            
+            //if sum<x keep adding from left.
             while(l<r && sum<x){
                 sum+=nums[l];
                 l++;
             }
-            // cout<<"After second  sum:"<<sum<<" l "<<l<<" r "<<r<<endl;
-            // cout<<sum<<endl;
-            // cout<<l<<" "<<r<<endl;
-            // cout<<n-r<<" right & left "<<l<<endl;
+            
+            //update ans if condition is satisfied
             if(sum==x){
-                // cout<<"Equal "<<sum<<" "<<x<<endl;
-                // cout<<"Equal "<<r<<" "<<l<<endl;
                 len=min(len,(n-r)+l);
             }
             
+            //keep moving backwards &  decrementing the answer
             if(r<n){
+                //decrement only if inside the range
                 sum-=nums[r];
             }
+            
+            //this is kept outside the if condition so that r can become n+1 & l can cover the whole array as well.(corner case)
             r++;
         }
+        
         return len==INT_MAX?-1:len;
     }
 };
