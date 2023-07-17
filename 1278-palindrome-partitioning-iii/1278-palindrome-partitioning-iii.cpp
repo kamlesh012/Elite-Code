e694f5a3f9ab911b29992abdceaa79df7d9b2dbf
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-07-17 09:24:52 +0530

    Time: 53 ms (27.24%), Space: 8.1 MB (34.55%) - LeetHub

--- 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.cpp
+++ 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.cpp
@@ -8,7 +8,8 @@ public:
             while(start<=end)if(s[start++]!=s[end--])cnt++;
             return cnt;
         };
-        
+        /*
+        //My Solution.
         vector<vector<vector<int>>> dp(101,vector<vector<int>>(101,vector<int>(101,-1)));
         
         function<int(int,int,int)> rec=[&](int start,int end,int cnt){
@@ -29,7 +30,37 @@ public:
             }
             return ans;
         };
-        
         return rec(0,0,k);
+        */
+        
+        //States can be reduced by taking few points in consideration.
+        //current end will always be one index before the next start.
+        //so instead of storing end we can calculate end from the value of next start.
+        //THe below approach is state reduced. but an error still persists. 
+        vector<vector<int>> dp(101,vector<int>(101,-1));
+        function<int(int,int)> rec=[&](int start,int cnt){
+            int n=s.length();
+            if(start==n){
+                if(!cnt)return 0;
+                else return (int)(1e9-1);
+            }
+            // else if(start>=n && cnt)return (int)10000-1;
+            
+            int &ans=dp[start][cnt];
+            // cout<<"ans "<<ans<<endl;
+            if(ans==-1){
+                ans=1e9-1;
+                for(int end=start;end<n;end++){
+                    int current_ans=1e9-1;
+                    // cout<<start<<" - "<<end<<endl;
+                    if(cnt)current_ans=check(start,end)+rec(end+1,cnt-1);
+                    // cout<<"curr ans "<<current_ans<<endl;
+                    ans=min(ans,current_ans);
+                }
+            }
+            return ans;
+        };
+        
+        return rec(0,k);
     }
 };
