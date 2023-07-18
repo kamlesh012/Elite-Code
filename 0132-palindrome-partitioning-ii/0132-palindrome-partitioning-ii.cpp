5d0efc3ba54fef79bb3c61f0832ec212c5d20803
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-07-18 12:25:37 +0530

    Time: 166 ms (79.59%), Space: 59.7 MB (14.91%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -6,24 +6,44 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<=i;j++){
                 ispal[j][i]=s[i]==s[j]?j+1>=i || ispal[j+1][i-1]:false;
-                // cout<<s.substr(j,i-j+1)<<endl;
-                // cout<<ispal[j][i]<<endl;
             }
-        };
-        vector<int> dp(n+1,-1);
-        function<int(int)> rec=[&](int i){
-          if(i==n)return 0;
-            if(dp[i]==-1){
-                int ans=1e5;
-                for(int j=i;j<n;j++){
-                    if(ispal[i][j])ans=min(ans,rec(j+1)+1);
-                }
-                return dp[i]=ans;
-            }
-            return dp[i];
         };
         
-        return rec(0)-1;
+        // vector<int> dp(n+1,-1);
+        // function<int(int)> rec=[&](int i){
+        //   if(i==n)return 0;
+        //     if(dp[i]==-1){
+        //         int ans=1e5;
+        //         for(int j=i;j<n;j++){
+        //             if(ispal[i][j])ans=min(ans,rec(j+1)+1);
+        //         }
+        //         return dp[i]=ans;
+        //     }
+        //     return dp[i];
+        // };
+        // return rec(0)-1;
+        
+        vector<int> dp(n+1,1e5);
+        dp[n]=0;
+        for(int i=n-1;i>=0;--i){
+            for(int j=i;j<n;j++){
+                if(ispal[i][j])dp[i]=min(dp[i],dp[j+1]+1);
+            }
+        }
+        return dp[0]-1;
+        
+        //2d-DP Failing in making states.
+//         vector<vector<int>> dp(n+1,vector<int>(n+1,1e5));
+        
+//         for(int i=n;i>=0;--i){
+//             int ans=1e5;
+//             for(int j=i;j<n;j++){
+//                 if(i==n)dp[j][i]=0;
+//                 else if(ispal[j][i])
+//                 dp[j][i]=min(dp[j][i],dp[j+1][i]+1);
+//             }
+//         }
+//         return dp[0][0];
         
     }
 };
