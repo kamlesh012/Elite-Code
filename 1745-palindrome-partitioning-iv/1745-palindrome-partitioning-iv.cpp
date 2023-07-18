37dd7410f822950115ead276ac6f20b6e3ad183c
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-07-18 11:11:50 +0530

    Time: 382 ms (67.92%), Space: 17.5 MB (62.63%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -4,7 +4,7 @@ public:
     bool checkPartitioning(string s) {
         int n=s.length();
         vector<vector<bool>> pal(n+1,vector<bool>(n+1));
-        
+        //O(n^3) brute force palindrome checker.->TLEd.
 //         for(int i=0;i<n;i++){
 //             for(int j=i;j<n;j++){
 //                 bool ok=true;
@@ -22,6 +22,7 @@ public:
         
         //Thanks Votrubac for this simple techinque.
         //Precomputing whether a substring is palindrome or not for all substrings
+        
         //Efficient appraoch using dp in O(n^2);
         for(int i=n-1;i>=0;--i){
             for(int j=i;j<n;j++){
@@ -29,31 +30,32 @@ public:
             }
         }
         
-        // for(int i=0;i<n;i++){
-        //     if(pal[0][i]){
-        //         for(int j=i+1;j<n;j++){
-        //             if(pal[i+1][j] && pal[j+1][n-1])return true;
-        //         }
-        //     }
-        // }
-        // return false;
+        //Votrubac's Iterative way to split string into three ways.(faster)AC
         
-        //My DP-TLEing.
-        vector<vector<int>> dp(n+1,vector<int>(10,-1));
-        
-        function<bool(int i,int cnt)> rec=[&](int i,int cnt){
-            if(i>=n)return !cnt;
-            if(dp[i][cnt]==-1){
-                bool ans=false;
-                for(int j=i;j<n;j++){
-                    if(pal[i][j] && cnt)ans|=rec(j+1,cnt-1);
+        for(int i=0;i<n;i++){
+            if(pal[0][i]){
+                for(int j=i+1;j<n;j++){
+                    if(pal[i+1][j] && pal[j+1][n-1])return true;
                 }
-                dp[i][cnt]=ans;
             }
-            bool curr=dp[i][cnt];
-            return curr;
-        };
-        return rec(0,3);
+        }
+        return false;
+        
+        //My Standard DP(slower)AC
+        // vector<vector<int>> dp(n+1,vector<int>(10,-1));
+        // function<bool(int i,int cnt)> rec=[&](int i,int cnt){
+        //     if(i>=n)return !cnt;
+        //     if(dp[i][cnt]==-1){
+        //         bool ans=false;
+        //         for(int j=i;j<n;j++){
+        //             if(pal[i][j] && cnt)ans|=rec(j+1,cnt-1);
+        //         }
+        //         dp[i][cnt]=ans;
+        //     }
+        //     bool curr=dp[i][cnt];
+        //     return curr;
+        // };
+        // return rec(0,3);
         
     }
 };
