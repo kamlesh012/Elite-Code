0eb90bb0b1e4367c15f83c88b74028e46c33d527
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-07-18 11:10:04 +0530

    Time: 650 ms (32.60%), Space: 22.5 MB (50.00%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -28,63 +28,32 @@ public:
                 pal[i][j]=(s[i]==s[j])?(i+1>=j || pal[i+1][j-1]):false;
             }
         }
-        //   for(int i=n-1;i>=0;--i){
-        //     for(int j=i;j>=0;--j){
-        //         pal[j][i]=(s[i]==s[j])?(j+1>=i || pal[j+1][i-1]):false;
+        
+        // for(int i=0;i<n;i++){
+        //     if(pal[0][i]){
+        //         for(int j=i+1;j<n;j++){
+        //             if(pal[i+1][j] && pal[j+1][n-1])return true;
+        //         }
         //     }
         // }
-        
-//         for(int end=n-1;end>=0;--end){
-//             for(int start=end;start>=0;--start){
-                
-//                 // pal[start][end]=(s[start]==s[end])?(start+1>=end || pal[start+1][end-1]):false;
-//                 bool ok=false;
-//                 if(s[start]==s[end]){
-//                     if(start==end)ok=true;
-//                     else if(start+1>=end || pal[start+1][end-1]==true)ok=true;
-//                 }
-//                 pal[start][end]=ok;
-                
-//                 if(start+1<=end){
-//                 cout<<start+1<<" between "<<end-1<<endl;
-//                 cout<<pal[start+1][end-1]<<endl;
-//                 }
-//                 cout<<start<<" "<<end<<endl;
-//                 cout<<s.substr(start,end-start+1)<<endl;
-//                 cout<<pal[start][end]<<endl;
-//             }
-//         }
-        
-        for(int i=0;i<n;i++){
-            // cout<<s.substr(0,i+1)<<endl;
-            // cout<<pal[0][i]<<endl;
-            if(pal[0][i]){
-                for(int j=i+1;j<n;j++){
-                    // cout<<s.substr(i+1,j-i)<<" "<<s.substr(j+1,n-j)<<endl;
-                    // cout<<i+1<<"-"<<j<<" ----- "<<j+1<<"-"<<n-1<<endl;
-                    // cout<<pal[i+1][j]<<" "<<pal[j+1][n-1]<<endl;
-                    if(pal[i+1][j] && pal[j+1][n-1])return true;
-                }
-            }
-        }
-        return false;
+        // return false;
         
         //My DP-TLEing.
-//         vector<vector<int>> dp(n+1,vector<int>(10,-1));
+        vector<vector<int>> dp(n+1,vector<int>(10,-1));
         
-//         function<bool(int i,int cnt)> rec=[&](int i,int cnt){
-//             if(i>=n)return !cnt;
-//             if(dp[i][cnt]==-1){
-//                 bool ans=false;
-//                 for(int j=i;j<n;j++){
-//                     if(pal[i][j] && cnt)ans|=rec(j+1,cnt-1);
-//                 }
-//                 dp[i][cnt]=ans;
-//             }
-//             bool curr=dp[i][cnt];
-//             return curr;
-//         };
-//         return rec(0,3);
+        function<bool(int i,int cnt)> rec=[&](int i,int cnt){
+            if(i>=n)return !cnt;
+            if(dp[i][cnt]==-1){
+                bool ans=false;
+                for(int j=i;j<n;j++){
+                    if(pal[i][j] && cnt)ans|=rec(j+1,cnt-1);
+                }
+                dp[i][cnt]=ans;
+            }
+            bool curr=dp[i][cnt];
+            return curr;
+        };
+        return rec(0,3);
         
     }
 };
