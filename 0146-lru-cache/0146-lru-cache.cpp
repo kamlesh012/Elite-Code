a5c722a215b568436c4c474b7da39ccf676441e3
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-07-21 01:38:44 +0530

    Time: 466 ms (85.98%), Space: 165.1 MB (77.26%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,4 +1,5 @@
 //Striver's Solution Using Self Created Doubly Linked List
+/*
 class LRUCache {
 public:
 class node
@@ -74,40 +75,39 @@ unordered_map<int,node*>mpp;
         mpp[_key] = head->next;
     }
 };
-
+*/
 //A concise solution using C++ STL
-/*
-class LRUCache{
-    size_t m_capacity;
-    unordered_map<int,  list<pair<int, int>>::iterator> m_map; //m_map_iter->first: key, m_map_iter->second: list iterator;
-    list<pair<int, int>> m_list;                               //m_list_iter->first: key, m_list_iter->second: value;
+// /*
+
+class LRUCache {
+    int capacity;
+    list<pair<int, int>> li;
+    unordered_map<int, list<pair<int, int>>::iterator> um; 
+
 public:
-    LRUCache(size_t capacity):m_capacity(capacity) {
-    }
+    LRUCache(int capacity) : capacity{capacity} {}
+    
     int get(int key) {
-        auto found_iter = m_map.find(key);
-        if (found_iter == m_map.end()) //key doesn't exist
-            return -1;
-        m_list.splice(m_list.begin(), m_list, found_iter->second); //move the node corresponding to key to front
-        return found_iter->second->second;                         //return value of the node
+        if (um.find(key) == um.end()) return -1;
+        li.splice(li.begin(), li, um[key]);     //MOve node to begin/head
+        return um[key]->second;
     }
-    void set(int key, int value) {
-        auto found_iter = m_map.find(key);
-        if (found_iter != m_map.end()) //key exists
-        {
-            m_list.splice(m_list.begin(), m_list, found_iter->second); //move the node corresponding to key to front
-            found_iter->second->second = value;                        //update value of the node
+    
+    void put(int key, int value) {
+        if (get(key) != -1) {
+            um[key]->second = value;
             return;
         }
-        if (m_map.size() == m_capacity) //reached capacity
-        {
-           int key_to_del = m_list.back().first; 
-           m_list.pop_back();            //remove node in list;
-           m_map.erase(key_to_del);      //remove key in map
+        
+        if (um.size() == capacity) {
+            int delKey = li.back().first;
+            li.pop_back();
+            um.erase(delKey);
         }
-        m_list.emplace_front(key, value);  //create new node in list
-        m_map[key] = m_list.begin();       //create correspondence between key and node
+        
+        li.emplace_front(key, value);
+        um[key] = li.begin();
     }
 };
 
-*/
+// */
