9e128d7c4e09b7bc66816e8d5e5ada77d4cece5d
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-07-27 08:34:47 +0530

    Time: 295 ms (6.25%), Space: 97.9 MB (5.17%) - LeetHub

---
+++ 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.cpp
@@ -0,0 +1,38 @@
+struct comp{
+    bool operator()(const int &a,const int &b)const{
+        return a>b;
+    }
+};
+class Solution {
+public:
+    bool canReorderDoubled(vector<int>& arr) {
+        int n=arr.size();
+        multiset<int> st;
+        multiset<int,comp> mst;
+        for(auto i:arr){
+            if(i>0)st.insert(i);
+            else mst.insert(i);
+        }
+        // sort(arr.begin(),arr.end());
+        
+        for(auto i:st){
+            auto it=st.find(2*i);
+            // cout<<i<<" "<<2*i<<" "<<*it<<endl;
+            if(it!=st.end())st.erase(it);
+            else return false;
+            
+            if(st.empty())break;
+        }
+        
+        for(auto i:mst){
+            auto it=mst.find(2*i);
+            // cout<<i<<" "<<2*i<<" "<<*it<<endl;
+            if(it!=mst.end())mst.erase(it);
+            else return false;
+            
+            if(mst.empty())break;
+        }
+        return true;
+        
+    }
+};
