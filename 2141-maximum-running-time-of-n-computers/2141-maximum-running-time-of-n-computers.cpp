b40b1dd05d94bb9def324c1d45062544a5a88b34
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-04 09:45:31 +0530

    Time: 159 ms (46.06%), Space: 57.5 MB (5.67%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -9,21 +9,13 @@ public:
             if(i<n)top.push_back(bat[i]);
             else extra+=bat[i];
         }
-        // sort(top.begin(),top.end());
         long long l=-1ll,h=total+1ll;
         while(h-l>1){
             long long mid=(h-l)/2+l;
-            // cout<<"mid "<<mid<<" l "<<l<<" h "<<h<<endl;
             long long left=0ll;
-            
-            // cout<<"TOP "<<endl;
-            for(auto i:top){
-                // cout<<i<<" ";
+            for(auto &i:top){
                 left+=max(0ll,mid-i);
             }
-            // cout<<endl;
-            
-            // cout<<" left "<<left<<" extra "<<extra<<endl;
             if(left>extra)h=mid;
             else l=mid;
         }
@@ -31,11 +23,7 @@ public:
         return l;
         
         
-        
-        
-        
-        
-        
+
         //Dry RUn on these.
 //             2
 //             [11,3,22]
@@ -43,6 +31,7 @@ public:
 //         5
 //         [1,6,7,8,9,15]
       /*  
+        sort(top.begin(),top.end());
         int runtime=0;
         for(int i=1;i<top.size();i++){
             int sub=(top[i]-top[i-1])*(i);   
