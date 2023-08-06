67b595d1310e288b019da832706be92cda5ce37e
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-06 12:55:55 +0530

    Time: 47 ms (37.32%), Space: 18.7 MB (23.60%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -4,28 +4,39 @@ public:
     int change(int amount, vector<int>& coins) {
         
         int n=coins.size();
-        vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
-        
-        function<int(int,int)> rec=[&](int i,int sum){
-            if(sum==0)return 1;
-            if(i==n)return 0;
-            
-            if(dp[i][sum]==-1)
-            {
-                int pick=0,notpick=0;
-                
-                //Skip current Coin
-                notpick=rec(i+1,sum);
-                
-                //Keep Choosing current coin till we can.
-                if(coins[i]<=sum)pick=rec(i,sum-coins[i]);
-                
-                dp[i][sum]=pick+notpick;
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,0));
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=amount;j++){
+                if(j==0)dp[i][j]=1;
+                else if(i==0)dp[i][j]=0;
+                else{
+                    dp[i][j]=dp[i-1][j];
+                    if(coins[i-1]<=j)dp[i][j]+=dp[i][j-coins[i-1]];
+                }
             }
-            return dp[i][sum];
-        };
+        }
+        return dp[n][amount];
+//         vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
+//         function<int(int,int)> rec=[&](int i,int sum){
+//             if(sum==0)return 1;
+//             if(i==n)return 0;
+            
+//             if(dp[i][sum]==-1)
+//             {
+//                 int pick=0,notpick=0;
+                
+//                 //Skip current Coin
+//                 notpick=rec(i+1,sum);
+                
+//                 //Keep Choosing current coin till we can.
+//                 if(coins[i]<=sum)pick=rec(i,sum-coins[i]);
+                
+//                 dp[i][sum]=pick+notpick;
+//             }
+//             return dp[i][sum];
+//         };
         
-        return rec(0,amount);
+//         return rec(0,amount);
         
         }
 };
