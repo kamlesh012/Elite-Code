57580217a45e42a8b889aaf4d1e2837a94d61fb8
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-06 13:40:20 +0530

    Time: 684 ms (29.38%), Space: 488.7 MB (5.01%) - LeetHub

--- 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.cpp
+++ 2585-number-of-ways-to-earn-points/2585-number-of-ways-to-earn-points.cpp
@@ -2,8 +2,8 @@
 class Solution {
 public:
     int waysToReachTarget(int target, vector<vector<int>>& types) {
-        
-        int n=types.size();
+        //2D Variation of Coin-Change II
+            int n=types.size();        
         vector<vector<vector<int>>> dp(51,vector<vector<int>>(51,vector<int>(target+1,-1)));
         function<int(int,int,int)> rec=[&](int i,int current,int sum){
             if(sum==0)return 1;
@@ -18,5 +18,30 @@ public:
         };
         
         return rec(0,0,target);
+        
+/*
+//Iterative Pending
+
+
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(51,vector<int>(target+1,0)));
+        int sol=0;
+        for(int i=0;i<=n;i++){
+            for(int c=0;c<51;c++){
+                for(int j=0;j<=target;j++){
+                    if(j==0)dp[i][c][j]=1;
+                    else if(i==0 || c==0)dp[i][c][j]=0;
+                    else{
+                        dp[i][c][j]=dp[i-1][0][j];
+                        
+                        if(c<types[i-1][0] && j>=types[i-1][1])
+                            dp[i][c][j]=(dp[i][c][j]%mod+dp[i][c+1][j-types[i-1][1]]%mod)%mod;
+                        sol=max(sol,dp[i][c][j]);
+                    }
+                }
+            }
+        }
+        return sol;
+        
+        */
     }
 };
