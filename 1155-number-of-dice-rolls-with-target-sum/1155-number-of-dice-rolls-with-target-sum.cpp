78efdee692152aae7366807d2100c459a5b3e138
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-06 14:39:44 +0530

    Time: 87 ms (16.84%), Space: 8.5 MB (48.30%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,59 @@
+#define mod 1000000007
+class Solution {
+public:
+    int numRollsToTarget(int n, int k, int target) {
+        
+        // vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(k+1,vector<int>(target+1,0)));
+        // vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(target+1,vector<int>(k+1,0)));
+        vector<vector<int>> dp(n+1,vector<int>(target+1,0));
+        
+        for(int i=0;i<=n;i++){
+            for(int t=0;t<=target;t++){
+                for(int j=0;j<=k;j++){
+//                     This fucking Base case was the culprit.
+                    if(t==0 && i==0)dp[i][t]=1;
+                    else if(t==0 || i==0 || j==0)continue;
+                        //same as dp[i][t]=0;
+                    else {
+                            if(j<=t)
+                                dp[i][t]=((dp[i][t]%mod)+(dp[i-1][t-j]%mod))%mod;
+                        }
+                }
+            }
+        }
+        return dp[n][target];
+        //Failed Approaches
+//         function<int(int,int)> rec=[&](int i,int k,int t){
+//             if(i==n){
+//                 if(t==0)
+//                 return 1;
+//                 else return 0;
+//             }
+//             if(t==0)return 1;
+            
+//             int &ans=dp[i][k][t];
+//             if(ans!=-1)return ans;
+//             int curr=0;
+//             // for(int j=1;j<=k;j++){
+//                 if(j<=t)curr=((curr%mod)+(rec(i+1,t-j)%mod))%mod;
+//             // }
+//             return ans=curr;
+//         };
+        
+//         return rec(0,target);
+        
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<=k;j++){
+        //         for(int t=0;t<=target;t++){           
+        //             if(t==0)dp[i][j][t]=1;
+        //             else if(j==0 || i==0)dp[i][j][t]=0;
+        //             else{
+        //                 if(t-k>=0)dp[i][j][t]+=dp[i-1][j][t-j];
+        //             }
+        //         }
+        //     }
+        // }
+        // return dp[n-1][k-1][target-1];
+            
+    }
+};
