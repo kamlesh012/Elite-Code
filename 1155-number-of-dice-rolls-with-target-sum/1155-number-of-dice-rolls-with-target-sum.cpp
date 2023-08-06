e5d12235b687f1077819e447bdcb35ff6883ff56
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-06 14:42:21 +0530

    Time: 38 ms (61.71%), Space: 8.7 MB (26.48%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -5,42 +5,48 @@ public:
         
         // vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(k+1,vector<int>(target+1,0)));
         // vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(target+1,vector<int>(k+1,0)));
-        vector<vector<int>> dp(n+1,vector<int>(target+1,0));
+        // vector<vector<int>> dp(n+1,vector<int>(target+1,0));
         
-        for(int i=0;i<=n;i++){
-            for(int t=0;t<=target;t++){
-                for(int j=0;j<=k;j++){
-//                     This fucking Base case was the culprit.
-                    if(t==0 && i==0)dp[i][t]=1;
-                    else if(t==0 || i==0 || j==0)continue;
-                        //same as dp[i][t]=0;
-                    else {
-                            if(j<=t)
-                                dp[i][t]=((dp[i][t]%mod)+(dp[i-1][t-j]%mod))%mod;
-                        }
-                }
-            }
-        }
-        return dp[n][target];
-        //Failed Approaches
-//         function<int(int,int)> rec=[&](int i,int k,int t){
-//             if(i==n){
-//                 if(t==0)
-//                 return 1;
-//                 else return 0;
+//         for(int i=0;i<=n;i++){
+//             for(int t=0;t<=target;t++){
+//                 for(int j=0;j<=k;j++){
+// //                     This fucking Base case was the culprit.
+//                     if(t==0 && i==0)dp[i][t]=1;
+//                     else if(t==0 || i==0 || j==0)continue;
+//                         //same as dp[i][t]=0;
+//                     else {
+//                             if(j<=t)
+//                                 dp[i][t]=((dp[i][t]%mod)+(dp[i-1][t-j]%mod))%mod;
+//                         }
+//                 }
 //             }
-//             if(t==0)return 1;
-            
-//             int &ans=dp[i][k][t];
-//             if(ans!=-1)return ans;
-//             int curr=0;
-//             // for(int j=1;j<=k;j++){
-//                 if(j<=t)curr=((curr%mod)+(rec(i+1,t-j)%mod))%mod;
-//             // }
-//             return ans=curr;
-//         };
+//         }
+//         return dp[n][target];
         
-//         return rec(0,target);
+        //Failed Approaches
+        vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
+        function<int(int,int)> rec=[&](int i,int t){
+            if(i==n && t==0)return 1;
+            else if(i==n || t==0)return 0;
+            
+            //THis f*ing base case was wrong.
+            // if(i==n){
+                // if(t==0)
+                // return 1;
+                // else return 0;
+            // }
+            // if(t==0)return 1;
+            
+            int &ans=dp[i][t];
+            if(ans!=-1)return ans;
+            int curr=0;
+            for(int j=1;j<=k;j++){
+                if(j<=t)curr=((curr%mod)+(rec(i+1,t-j)%mod))%mod;
+            }
+            return ans=curr;
+        };
+        
+        return rec(0,target);
         
         // for(int i=0;i<=n;i++){
         //     for(int j=0;j<=k;j++){
