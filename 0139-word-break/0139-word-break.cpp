d8dcdd189f1e527fc627f6a2ce6c1d4272975797
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-06 09:35:40 +0530

    Time: 9 ms (59.88%), Space: 8.3 MB (74.84%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -7,30 +7,33 @@ public:
             mp[i]++;
         }
         int n=s.size();
-        vector<bool> dp(n+1,0);
-        dp[n]=true;
-        for(int start=n-1;start>=0;--start){
-            string temp;
-            for(int end=start;end<n;end++){
-                temp.push_back(s[end]);
-                bool found=mp.count(temp);
-                dp[start]=dp[start]|(found && dp[end+1]);
-            }
-        }
-        return dp[0];
+        // vector<bool> dp(n+1,0);
+        // dp[n]=true;
+        // for(int start=n-1;start>=0;--start){
+        //     string temp;
+        //     for(int end=start;end<n;end++){
+        //         temp.push_back(s[end]);
+        //         bool found=mp.count(temp);
+        //         dp[start]=dp[start]|(found && dp[end+1]);
+        //     }
+        // }
+        // return dp[0];
         
       // DP[i] represents whether the substring  starting at position is valid or not.
-      //   function<int(int)> rec=[&](int i){
-      //       if(i==n)return 0;
-      //       int ok=1;
-      //       string temp;
-      //     for(int j=0;j+i<n;j++){
-      //         temp.push_back(s[j]);
-      //         int f=mp.count(temp);
-      //         if(f)ok=ok&rec(i+j+1);
-      //     }
-      //       return ok;
-      //   };
-      //   return rec(0);
+        vector<int> dp(n,-1);
+        function<int(int)> rec=[&](int i){
+            if(i==n)return 1;
+            if(dp[i]==-1){
+                dp[i]=0;
+                string temp;
+              for(int j=0;j+i<n;j++){
+                  temp.push_back(s[i+j]);
+                  int found=mp.count(temp);
+                  dp[i]|=(found&rec(i+j+1));
+              }
+            }
+            return dp[i];
+        };
+        return rec(0);
     }
 };
