e2e8bdb5bb53d321c53842bf56182a033fe3c049
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-07 08:51:29 +0530

    Time: 229 ms (40.38%), Space: 127.1 MB (10.19%) - LeetHub

--- 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.cpp
+++ 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minOperations(vector<int>& a, vector<int>& b) {
-        
+       // https://leetcode.com/problems/equal-sum-arrays-with-minimum-number-of-operations/discuss/1086127/greedy-C%2B%2B-or-O(N)-time-and-O(1)-space-or-short-and-easy-to-understand-or-100-beat
         int s1=accumulate(a.begin(),a.end(),0);
         int s2=accumulate(b.begin(),b.end(),0);
         if(s1>s2){
