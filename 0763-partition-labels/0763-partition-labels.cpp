c73caa2b2066f028a22a2a11e24c6a09f6d97e5c
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-08 13:34:50 +0530

    Time: 3 ms (80.01%), Space: 7.3 MB (5.75%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        map<char,int> start,end;
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            if(!start.count(s[i]))start[s[i]]=i+1;
+            end[s[i]]=i+1;
+        }
+        vector<pair<int,int>> intervals;
+        for(int i=0;i<26;i++){
+            char c=i+'a';
+            if(start[c])
+            intervals.push_back({start[c],end[c]});
+        }
+        sort(intervals.begin(),intervals.end(),[&](pair<int,int> &a,pair<int,int> &b){
+            if(a.first==b.first)return a.second<b.second;
+            return a.first<b.first;
+        });
+        int strt=intervals[0].first,endd=intervals[0].second;
+        
+        vector<int> ans;
+        
+        // for(auto i:intervals){
+        //     cout<<i.first<<" "<<i.second<<endl;
+        // }
+        // cout<<endl;
+        
+        bool last=false;
+        for(int i=1;i<intervals.size();i++){
+            if(endd<intervals[i].first){
+                if(i==intervals.size()-1)last=true;
+                ans.push_back(endd-strt+1);
+                strt=intervals[i].first;
+                endd=intervals[i].second;
+            }
+            else{
+                endd=max(endd,intervals[i].second);
+            }
+        }
+        // if(!last)
+            ans.push_back(endd-strt+1);
+        return ans;
+            
+    }
+};
