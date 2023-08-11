b1e172ec274dc36a1cc3240356e96c484e1b6626
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-11 18:43:36 +0530

    Time: 864 ms (5.01%), Space: 225.1 MB (5.82%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> num(n);
+        iota(num.begin(),num.end(),1);
+        vector<vector<int>> ans;
+        
+     function<void(int i,int left,vector<int>)> knapsack=[&](int i,int left,vector<int> temp){
+         if(!left){
+             ans.push_back(temp);
+             return;
+         }
+         if(i==n)return;
+         
+         knapsack(i+1,left,temp);
+         
+             
+         temp.push_back(num[i]);
+            knapsack(i+1,left-1,temp);
+            temp.pop_back();
+     };
+        vector<int> temp;
+        knapsack(0,k,temp);
+        return ans;
+    }
+};
