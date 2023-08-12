7ca995834854e6bfda8fe854b5dc81432ebb30e0
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-12 19:43:02 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (65.86%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& cdt, int target) {
-        //Brute-Force Recursive Knapsack.
         
+        /*
+        //Brute-Force Recursive Knapsack.  
      vector<vector<int>> master;
         function<void(vector<int> &temp,int i,int sum)> rec=[&](vector<int> &temp,int i,int sum){
             if(sum==0){
@@ -24,6 +25,32 @@ public:
         
         vector<int> temp;
         rec(temp,0,target);
+        return master;
+        */
+        
+        //Classical Backtracking Method.
+        //Try to visualise in form of levels.
+        //for each level a element can be chosen from nums[ind] to nums[n-1]
+        
+         vector<vector<int>> master;        
+        function<void(int ind,vector<int> &temp,int sum)> rec=[&](int ind,vector<int> &temp,int sum){
+            if(sum==0){
+                master.push_back(temp);
+                return;
+            }
+            if(ind==cdt.size())return;
+            
+            for(int i=ind;i<cdt.size();i++){
+                if(cdt[i]<=sum){
+                    temp.push_back(cdt[i]);
+                    rec(i,temp,sum-cdt[i]);
+                    temp.pop_back();
+                }
+            }
+        };
+        
+        vector<int> temp;
+        rec(0,temp,target);
         return master;
     }
 };
