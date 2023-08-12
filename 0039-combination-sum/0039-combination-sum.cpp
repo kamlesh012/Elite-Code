81087e7a0ec00bcb0628c085a80301c9f0b18022
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-12 18:41:16 +0530

    Time: 50 ms (23.31%), Space: 24.1 MB (25.10%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& cdt, int target) {
+        
+     vector<vector<int>> master;
+        function<void(vector<int> &temp,int i,int sum)> rec=[&](vector<int> temp,int i,int sum){
+            if(sum==0){
+                master.push_back(temp);
+                return;
+            }
+            if(i==cdt.size())return;
+            //not pick
+            rec(temp,i+1,sum);
+            
+            //pick
+            if(cdt[i]<=sum)
+            {
+                temp.push_back(cdt[i]);
+                rec(temp,i,sum-cdt[i]);
+                temp.pop_back();
+            }
+        };
+        
+        vector<int> temp;
+        rec(temp,0,target);
+        return master;
+    }
+};
