070d33339f97bbb39dd52fcbc137f6ea3a3923e5
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-12 13:07:28 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (59.91%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -33,8 +33,8 @@ public:
     
     
     vector<vector<int>> permute(vector<int>& nums) {
-        
-        //Standard BackTracking Approach
+    /*    
+        //Standard BackTracking Approach-1
         
          //Stores the answer
         vector<vector<int>> master;
@@ -68,10 +68,30 @@ public:
             }
 
         };
+        
         vector<int> temp;
         solve(temp);
         return master;
+     */
+        vector<vector<int>> ans;
         
+        //Standard BackTracking Approach-2
+        //Here in this approach we will swap each element of nums with a specific index (say ind) once & in next recursive call we will start from the index ind+1 so that we don't swap back the values we swapped in previous recursive call.
+        
+        function<void(int ind)> rec=[&](int ind){
+            if(ind==nums.size()){
+                ans.push_back(nums);
+                return;
+            }
+            //Remember to start i from ind always so that we don't swap values back
+            for(int i=ind;i<nums.size();i++){
+                swap(nums[i],nums[ind]);
+                rec(ind+1);
+                swap(nums[i],nums[ind]);
+            }
+        };
+        rec(0);
+        return ans;
         
         //         My Non-STandard Approach Using Next Permutation
         /*
