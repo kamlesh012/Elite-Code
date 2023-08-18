8dda544666734b92d4a19a5d5b037e3e06b3d510
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-18 10:13:29 +0530

    Time: 8 ms (41.52%), Space: 6 MB (79.18%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    int total=0;
+    bool validate(int r,int c,vector<string> &board){
+        int n=board.size();
+        for(int i=0;i<n;i++){
+            if(board[r][i]=='Q')return false;
+            if(board[i][c]=='Q')return false;
+        }
+        
+        int x=r,y=c;
+        while(x>=0 && x<n && y>=0 && y<n){
+            if(board[x][y]=='Q')return false;
+            x++;
+            y++;
+        }
+        
+        x=r,y=c;
+        while(x>=0 && x<n && y>=0 && y<n){
+            if(board[x][y]=='Q')return false;
+            x--;
+            y--;
+        }
+        
+        x=r,y=c;
+        while(x>=0 && x<n && y>=0 && y<n){
+            if(board[x][y]=='Q')return false;
+            x--;
+            y++;
+        }
+        
+        x=r,y=c;
+        while(x>=0 && x<n && y>=0 && y<n){
+            if(board[x][y]=='Q')return false;
+            x++;
+            y--;
+        }
+        return true;
+    }
+    //Solving Row by Row.
+    //FOr first row,validating each cell & then proceeding to next row after validating each cell.
+    void solve(int row,vector<string> &board){
+          if(row==board.size()){
+              total++;
+              return;
+          }
+          for(int col=0;col<board.size();col++){
+              bool ok=validate(row,col,board);
+              if(ok){
+                  board[row][col]='Q';
+                  solve(row+1,board);
+                  board[row][col]='.';
+              }
+          }    
+        }
+    int totalNQueens(int n) {
+        string s(n,'.');
+        vector<string> board;
+        for(int i=0;i<n;i++)
+            board.push_back(s);
+        solve(0,board);
+        return total;
+    }
+};
