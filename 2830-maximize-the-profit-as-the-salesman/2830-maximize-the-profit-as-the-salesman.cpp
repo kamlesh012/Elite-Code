c7859f70a30014f21d0c1d7dd60980c24a04ef0f
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-21 15:17:50 +0530

    Time: 588 ms (66.67%), Space: 178.8 MB (50.00%) - LeetHub

--- 2830-maximize-the-profit-as-the-salesman/2830-maximize-the-profit-as-the-salesman.cpp
+++ 2830-maximize-the-profit-as-the-salesman/2830-maximize-the-profit-as-the-salesman.cpp
@@ -1,12 +1,47 @@
 class Solution {
 public:
+    
     int maximizeTheProfit(int n, vector<vector<int>>& a) {
         
+        //An Amazing O(n) DP Solution
+        //Thanks Pawan Kumar Giri.
+        
+        map<int,vector<pair<int,int>>> range;
+        //Storing all offers that start at index i  with their end points & gold coins.
+        for(auto i:a){
+            range[i[0]].push_back({i[1],i[2]});
+        }
+        
+        vector<int> dp(n,0);
+        
+        //Now iterating over each house
+        for(int i=0;i<n;i++){
+            
+            //worst case answer will be the answer of previous house.
+            if(i)dp[i]=max(dp[i],dp[i-1]);
+            
+            //for all the offers that start at current index
+            for(auto v:range[i]){
+                
+                int end=v.first,gold=v.second;
+                
+    //set dp values for offers that start at current index & finish at end index.
+                if(i)dp[end]=max(dp[end],dp[i-1]+gold);
+                
+                else dp[end]=max(dp[end],gold);
+                        
+            }
+        }
+        return dp[n-1];
+        
+        
+        /*
+        
+        //DP+Binary Search O(nlog(n))
         //Sorting on the basis of starting time, so that we can apply binary search 
         //to find index of the next offer  that has starting time greater than ending time of current offer.
         sort(a.begin(),a.end());
         vector<int> dp(a.size()+1,-1);
-        
         
         //Storing starting point of offers in sorted order so that binary search can be applied on this array to get index of required offer.
         //That same index can be use to access the corresponding offer in 2d vector named a.
@@ -27,6 +62,6 @@ public:
         };
         
         return rec(0);
-        
+        */
     }
 };
