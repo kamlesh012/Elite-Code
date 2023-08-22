2b18b2d11eabd30fab84b1b0e1a099c341561b9d
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-22 09:52:42 +0530

    Time: 302 ms (76.57%), Space: 221.1 MB (47.55%) - LeetHub

--- 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.cpp
+++ 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.cpp
@@ -14,23 +14,17 @@ public:
             int r=1;
             if(ind[i].size()){
                 int curr=1,total_diff=0;
-                cout<<i<<endl;
                 while(r<ind[i].size()){
                     int current_difference=ind[i][r]-ind[i][r-1]-1;
                     total_diff+=current_difference;
-                    // cout<<" cd "<<current_difference<<endl;
-                    // cout<<" tdb "<<total_diff<<" k "<<k<<endl;
                     while(l<r && total_diff>k){
                         int sub=ind[i][l+1]-ind[i][l]-1;
-                        // cout<<" sub "<<sub<<endl;
                         total_diff-=(sub);
                         l++;
                     }
-                    // cout<<" tda "<<total_diff<<endl;
                     curr=max(curr,r-l+1);
                     r++;
                 }
-                // cout<<i<<" "<<curr<<endl;
                 ans=max(ans,curr);
             }
         }
