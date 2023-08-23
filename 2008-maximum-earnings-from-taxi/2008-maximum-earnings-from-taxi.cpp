3170cbc3558fae8df1decf968c6ad2bdfc9bb728
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-23 09:39:59 +0530

    Time: 402 ms (97.03%), Space: 163.3 MB (7.38%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
@@ -1,33 +1,47 @@
 class Solution {
 public:
     long long maxTaxiEarnings(int n, vector<vector<int>>& a) {
-         int m=a.size();
-        for(int i=0;i<m;i++){
-            a[i][2]+=(a[i][1]-a[i][0]);
-        }
-        
-        //Storing starting point of offers in sorted order so that binary search can be applied on this array to get index of required offer.
-        //That same index can be use to access the corresponding offer in 2d vector named a.
-        sort(a.begin(),a.end());
-        
-        vector<long long> dp(m,-1);
-        vector<int> start;
-        
+        vector<vector<pair<long long,long long>>> points(n+1);
+        vector<long long> dp(n+1,0ll);
         for(auto i:a){
-            start.push_back(i[0]);
+            points[i[0]].push_back({i[1],i[2]+(i[1]-i[0])});
         }
         
-        function<long long(int i)> rec=[&](int i){
-            if(i>=a.size()) return 0ll;
-            if(dp[i]!=-1) return dp[i];
+        for(int i=1;i<=n;i++){
+            dp[i]=max(dp[i],dp[i-1]);
+            for(auto v:points[i]){
+                dp[v.first]=max(dp[v.first],dp[i]+v.second);
+            }
+        }
+        return dp[n];
+        
+//          int m=a.size();
+//         for(int i=0;i<m;i++){
+//             a[i][2]+=(a[i][1]-a[i][0]);
+//         }
+        
+//         //Storing starting point of offers in sorted order so that binary search can be applied on this array to get index of required offer.
+//         //That same index can be use to access the corresponding offer in 2d vector named a.
+//         sort(a.begin(),a.end());
+        
+//         vector<long long> dp(m,-1);
+//         vector<int> start;
+        
+//         for(auto i:a){
+//             start.push_back(i[0]);
+//         }
+        
+//         function<long long(int i)> rec=[&](int i){
+//             if(i>=a.size()) return 0ll;
+//             if(dp[i]!=-1) return dp[i];
             
-            long long nopick=rec(i+1);
-            int ind=lower_bound(start.begin(),start.end(),a[i][1])-start.begin();
-            long long pick=a[i][2]+rec(ind);
+//             long long nopick=rec(i+1);
+//             int ind=lower_bound(start.begin(),start.end(),a[i][1])-start.begin();
+//             long long pick=a[i][2]+rec(ind);
             
-            return dp[i]=max(pick,nopick);
-        };
-        long long ans= rec(0);
-        return ans;
+//             return dp[i]=max(pick,nopick);
+//         };
+//         long long ans= rec(0);
+//         return ans;
     }
 };
