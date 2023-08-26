30829e982f6d1b12e85285c652e85b67caae97df
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-26 14:15:35 +0530

    Time: 3 ms (23.59%), Space: 6.1 MB (38.95%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.cpp
+++ 0233-number-of-digit-one/0233-number-of-digit-one.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
+//         Digit DP - Clean Solution
     int countDigitOne(int n) {
-        
         int dp[10][2][10];
         memset(dp,-1,sizeof dp);
-        string upper=to_string(n);
+        string upper_limit=to_string(n);
         
-        function<int(int i,int turn,int one)> digit=[&](int i,int turn,int one){
-            if(i==upper.size())return one;
-    
-            int &ans=dp[i][turn][one];
+        /*
+         dp[i][tight][countOnes] = Count of all ones in position range [i....n-1] of all numbers till "upper_limit",based on current position i is tight bound or not
+ and count of ones till now from positions [0...i-1] are "countOnes".
+  */
+        function<int(int i,int tight,int countOnes)> digit=[&](int i,int tight,int countOnes){
+            if(i==upper_limit.size())return countOnes;
+            int &ans=dp[i][tight][countOnes];
             if(ans!=-1)return ans;
             
             ans=0;            
-            char up=turn?'9':upper[i];
+            char up=tight?'9':upper_limit[i];
             for(char c='0';c<=up;c++){
-                if(c=='1')ans+=digit(i+1,turn|(c!=up),one+1);
-                else ans+=digit(i+1,turn|(c!=up),one);
+                if(c=='1')ans+=digit(i+1,tight|(c!=up),countOnes+1);
+                else ans+=digit(i+1,tight|(c!=up),countOnes);
             }
             return ans;
         };
