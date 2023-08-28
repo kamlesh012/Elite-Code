7ed04bf26cfce0620ef9e44e8ba8d37f75b186a0
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-08-29 02:06:47 +0530

    Time: 13 ms (22.01%), Space: 22.1 MB (86.92%) - LeetHub

---
+++ 2835-minimum-operations-to-form-subsequence-with-target-sum/2835-minimum-operations-to-form-subsequence-with-target-sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minOperations(vector<int>& v, int target) {
+        
+        int n=v.size();
+        long long total_sum=accumulate(v.begin(),v.end(),0ll);
+        if(total_sum<target)return -1;
+        
+        sort(v.begin(),v.end());
+        
+        //Sample Test Case: (Iterate from right towards left)
+        //1 2 4 8 16       target =13
+        
+        //skip 16, take 8 & 4     now target=1
+        //skip 2,  take 1.      
+        
+        //0 Operations Required.
+        
+        int i=n-1,ops=0;
+        while(target>0){
+            int curr=v.back();
+            v.pop_back();
+
+            long long rem=total_sum-curr;
+            if(rem>=target){
+                total_sum-=curr;
+            }
+            else if(curr<=target){
+                total_sum-=curr;
+                target-=curr;
+            }
+            else{
+                v.push_back(curr/2);
+                v.push_back(curr/2);
+                ops++;
+            }
+        }
+        
+        return ops;
+    }
+};
