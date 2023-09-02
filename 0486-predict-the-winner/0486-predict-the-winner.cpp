1382411f77658c4422ced3d28cb20cf6e8128d8f
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-09-02 10:19:40 +0530

    Time: 4 ms (54.58%), Space: 7.8 MB (16.21%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    int ans(vector<int>& nums,int i,int j){
-        if(i==j)return nums[i];
-        if(i>j)return 0;
-        int a=nums[i]+min(ans(nums,i+2,j),ans(nums,i+1,j-1));
-        int b=nums[j]+min(ans(nums,i+1,j-1),ans(nums,i,j-2));
-        return max(a,b);
-    }
-    
-    bool PredictTheWinner(vector<int>& nums) {
-        int one=ans(nums,0,nums.size()-1);
-        
-        int total=0;
-        for(int i:nums)total+=i;
-        
-        return one>=total-one;
-        
-        
+    bool predictTheWinner(vector<int>& piles) {
+        int n=piles.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        function<int(int,int,bool)> rec=[&](int i,int j,bool turn){
+            if(i>j)return 0;
+            if(dp[i][j]!=-1)return dp[i][j];
+            if(turn){
+                dp[i][j]=max(rec(i+1,j,!turn)+piles[i],rec(i,j-1,!turn)+piles[j]);
+            }
+          else  dp[i][j]=min(rec(i+1,j,!turn)-piles[i],rec(i,j-1,!turn)-piles[j]);
+            return dp[i][j];
+        };
+        int ans=rec(0,n-1,1);
+        cout<<ans<<endl;
+        return ans>=0;
     }
 };
