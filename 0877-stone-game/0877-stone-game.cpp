1f3458ab6be7306236d32ae3a47d2161ce1a2185
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-09-02 10:20:49 +0530

    Time: 55 ms (11.16%), Space: 16.7 MB (12.62%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
         //Observation:
-        return true;
+        // return true;
         
         //OR using DP;
         
@@ -21,16 +21,18 @@ public:
         //Now Overall-> 3-1+2-1
         //here + as alice's score - are bob's score.
         //we can see alice is clearly the winner here.
-        
+        //Mini-Max Algo
         function<int(int,int,bool)> rec=[&](int i,int j,bool turn){
             if(i>j)return 0;
             if(dp[i][j]!=-1)return dp[i][j];
             if(turn){
                 dp[i][j]=max(rec(i+1,j,!turn)+piles[i],rec(i,j-1,!turn)+piles[j]);
             }
-          else  dp[i][j]=max(rec(i+1,j,turn)-piles[i],rec(i,j-1,!turn)-piles[j]);
+          else  dp[i][j]=min(rec(i+1,j,!turn)-piles[i],rec(i,j-1,!turn)-piles[j]);
             return dp[i][j];
         };
-        return rec(0,n-1,1)>0;
+        int ans=rec(0,n-1,1);
+        // cout<<ans<<endl;
+        return ans>=0;
     }
 };
