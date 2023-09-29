6ae7fcb750c20680650003e8d18f0162c2f52d9b
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-09-29 13:02:34 +0530

    Time: 552 ms (11.60%), Space: 318 MB (5.12%) - LeetHub

---
+++ 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<bool> canMakePaliQueries(string s, vector<vector<int>>& q) {
+        int n=s.length();
+        
+        vector<vector<int>> freq(n,vector<int>(26,0));
+        vector<int> prev(60,0);
+        
+        for(int i=0;i<n;i++){
+            freq[i]=prev;
+            freq[i][s[i]-'a']++;
+            prev=freq[i];
+        }
+        
+        vector<bool> ans;
+        for(auto v:q){
+            
+            int left=v[0],right=v[1],k=v[2];
+            int cnt=0,len=right-left+1;
+            vector<int> prevf(26,0);
+            if(left-1>=0){
+                prevf=freq[left-1];
+            }
+            int odd=0;
+            
+            // for(auto i:prevf){
+            //     cout<<i<<" ";
+            // }
+            // cout<<endl;
+            
+            for(int i=0;i<26;i++){
+                odd+=((freq[right][i]-prevf[i])&1);
+            }
+            // cout<<odd<<endl;
+            // for(int i=left;i<=right;i++){
+            //     freq[s[i]-'a']++;
+            // }
+            // for(auto i:freq){
+            //     if(i&1)odd++;
+            // }
+            ans.push_back(((2*k)+(len%2))>=odd);
+        }
+        
+        return ans;
+        
+    }
+};
