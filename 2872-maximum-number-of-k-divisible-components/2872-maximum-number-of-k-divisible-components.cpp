2b03514a514874cbc8ae654ab27225d67341abe8
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-10-01 19:25:45 +0530

    Time: 402 ms (86.96%), Space: 191.9 MB (34.78%) - LeetHub

--- 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
+++ 2872-maximum-number-of-k-divisible-components/2872-maximum-number-of-k-divisible-components.cpp
@@ -7,13 +7,11 @@ public:
             adjl[i[0]].push_back(i[1]);
             adjl[i[1]].push_back(i[0]);
         }
-        // vector<int> vis(n,0);
         int cnt=0ll;
-        
-        function<int (int src,int parent)> dfs=[&](int src,int parent){
+
+        function<int(int,int)> dfs=[&](int src,int parent){
             int sum=values[src];
             for(auto i:adjl[src]){
-                // if(i!=prnt && vis[i]==0){
                 if(i!=parent){
                     sum+=dfs(i,src);
                     sum%=k;
