fe43847b86978a8a104fc188480301c0e7a7dd7f
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2024-01-02 18:11:15 +0530

    Time: 378 ms (39.87%), Space: 24.5 MB (70.57%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -11,7 +11,6 @@ public:
             
            int j=i+1,k=n-1;        
             while(j<k){
-                
                 if((nums[j]+nums[k]+nums[i])<0){
                     j++;
                 }
@@ -22,16 +21,14 @@ public:
                     ans.push_back({nums[i],nums[j],nums[k]});
                     j++;
                     k--;
+                    
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
                 }
                 
             }
         }
         
-        set<vector<int>> st(ans.begin(),ans.end());
-        ans.clear();
-        for(auto i:st){
-            ans.push_back(i);
-        }
         
         return ans;
     }
