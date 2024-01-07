6642d8b1d5543344d30898353921085b9e3740be
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2024-01-07 23:34:12 +0530

    Time: 233 ms (73.26%), Space: 120.8 MB (70.70%) - LeetHub

--- 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.cpp
+++ 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.cpp
@@ -4,7 +4,7 @@ public:
         
         long long ans=0,sum=0;
         
-        map<int,int> mp;
+        unordered_map<int,int> mp;
         mp[0]++;
         
         for(int i=0;i<nums.size();i++){
