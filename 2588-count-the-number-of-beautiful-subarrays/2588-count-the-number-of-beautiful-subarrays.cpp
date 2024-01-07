d4ea52c437fb59aa5c21e632534cca8a7cab4dea
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2024-01-07 23:34:00 +0530

    Time: 307 ms (39.56%), Space: 122.8 MB (49.45%) - LeetHub

---
+++ 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long beautifulSubarrays(vector<int>& nums) {
+        
+        long long ans=0,sum=0;
+        
+        map<int,int> mp;
+        mp[0]++;
+        
+        for(int i=0;i<nums.size();i++){
+            sum^=nums[i];
+            ans+=mp[sum]++;
+        }
+        
+        return ans;
+    }
+};
