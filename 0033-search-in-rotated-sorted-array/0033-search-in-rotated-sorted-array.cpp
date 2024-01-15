4a67647dd06360e670a68d8197cb2af6831112e1
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2024-01-15 17:52:36 +0530

    Time: 0 ms (100.00%), Space: 11.4 MB (70.14%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,8 +6,15 @@ public:
         int l=-1,h=n,pivot=-1;
         while(h-l>1){
             int mid=(h-l)/2+l;
-            if(a[mid]>a[n-1])l=mid;
-            else h=mid;
+              //         The condition a[mid]<a[high] is based on the fact that in a sorted array the next 
+            // index element is always greater than the current element,j
+//             so if a[mid]<a[high] that means the subarray from mid-high is sorted.
+//             & to find the minimum we will check in the left side of mid(including mid).
+            if(a[mid]<=a[n-1])h=mid;
+            else l=mid;
+            
+            // if(a[mid]>a[n-1])l=mid;
+            // else h=mid;
         }
         pivot=h;
         
@@ -17,6 +24,7 @@ public:
         
         while(h-l>1){
             int mid=(h-l)/2+l;
+          
             if(a[mid]<target)l=mid;
             else h=mid;
         }
