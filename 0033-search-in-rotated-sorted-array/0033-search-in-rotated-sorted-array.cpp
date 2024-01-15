a2afb469519c9d6090511c8c65844172731b58dd
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2024-01-15 17:53:49 +0530

    Time: 0 ms (100.00%), Space: 11.3 MB (98.30%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -10,11 +10,18 @@ public:
             // index element is always greater than the current element,j
 //             so if a[mid]<a[high] that means the subarray from mid-high is sorted.
 //             & to find the minimum we will check in the left side of mid(including mid).
+            
+//             a[mid]<=a[n-1] has been used instead of a[high] because h=n, in first iteration
+//             because of invariant binary search.
+            
+//             Predicate 1:
             if(a[mid]<=a[n-1])h=mid;
             else l=mid;
             
+//             PReidcated 2:
             // if(a[mid]>a[n-1])l=mid;
             // else h=mid;
+//             Both preidcates will work, both mean the same
         }
         pivot=h;
         
