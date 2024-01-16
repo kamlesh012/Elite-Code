2dd8cca930f5c08cd88fde2b99298d6d00d1198d
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2024-01-16 10:33:01 +0530

    Time: 8 ms (23.74%), Space: 12.7 MB (32.21%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -4,11 +4,15 @@ public:
         int n=a.size();
         int l=0,h=n-1;
         while(l+1<n && a[l]==a[l+1])l++;
-        // while(h>l && a[h]==a[h-1])h--;
         
         while(l<h){
             int mid=(h-l)/2+l;
-            if(a[mid]<=a[h])h=mid;
+            // if(a[mid]<=a[h])h=mid;       //my working condition
+            
+            if(a[mid]<a[h])h=mid;           //the next two conditions work as the first one
+            else if(a[mid]==a[h])h--;
+            
+            
             else l=mid+1;
         }
         return a[h];
