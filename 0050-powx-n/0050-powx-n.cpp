b66e0eea14a9c2abb786d3ee2f542162c1be2f55
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2024-01-18 17:26:15 +0530

    Time: 3 ms (43.32%), Space: 6.4 MB (34.94%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        // if(x==1.000)return x;
         if(n==0)return 1;
         function<double(double num,long long pow)> binexpo=[&](double num,long long pow){
             if(pow==1)return num;
-            
             double curr=1.00;
             if(pow&1)curr*=num;
             double solution=binexpo(num,pow/2);
-            // cout<<num<<" "<<pow<<" "<<curr<<" "<<solution<<endl;
             curr=curr*solution*solution;
             return curr;
         };
