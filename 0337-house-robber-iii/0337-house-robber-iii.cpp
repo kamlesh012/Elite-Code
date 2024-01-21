b4e81f5a4ffa8e49bbaba1eea29bef752863ebd0
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2024-01-21 10:39:36 +0530

    Time: 23 ms (22.52%), Space: 32.3 MB (5.59%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rob(TreeNode* root) {
+        map<TreeNode*,map<bool,int>> dp;
+        
+        function<int(TreeNode *rt,bool flag)> memo=[&](TreeNode *rt,bool flag){
+            if(!rt)return 0;
+            if(dp.find(rt)!=dp.end() && dp[rt].find(flag)!=dp[rt].end())return dp[rt][flag];
+            
+            int pick=0,notpick=0;
+            if(flag)pick=(rt->val)+memo(rt->left,false)+memo(rt->right,false);
+            notpick=memo(rt->left,true)+memo(rt->right,true);
+            return dp[rt][flag]=max(pick,notpick);
+        };
+        return memo(root,true);
+    }
+};
