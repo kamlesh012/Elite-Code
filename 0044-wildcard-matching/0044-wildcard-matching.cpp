d08b336291b8e0811a15b1bfedea25f27cc57cda
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2024-01-25 11:38:32 +0530

    Time: 88 ms (39.06%), Space: 33.4 MB (5.31%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    // int dp[2001][2001]={-1};
+    bool isMatch(string s, string p,int i=0,int j=0) {
+        int n=s.length(),m=p.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        
+        function<int(int i,int j)> mem=[&](int i,int j){
+            if(i==n){
+                if(j==m)return 1;
+                if(p[j]!='*')return 0;
+                else {
+                    if(dp[i][j]!=-1)return dp[i][j];
+                    else return dp[i][j]=mem(i,j+1);
+                }
+            }
+            if(j==m)return 0;
+            if(dp[i][j]!=-1)return dp[i][j];
+
+            if(i<n && j<m && s[i]==p[j]){
+                return dp[i][j]=mem(i+1,j+1);
+            }
+            else{
+                bool pick=false,npick=false,skip=false;
+                if(p[j]=='*'){
+                    pick=mem(i+1,j);
+                    npick=mem(i+1,j+1);
+                    skip=mem(i,j+1);
+                    return dp[i][j]= pick || npick || skip;
+                }
+                else if(p[j]=='?')return dp[i][j]=mem(i+1,j+1);
+                else return dp[i][j]=0;
+            }    
+        };
+        
+        return mem(0,0);
+        //      abcdefdcg   
+        //      abc*dc?*
+        
+    }
+};
