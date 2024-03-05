5abf75f0eadaa217a6edf88cd132ef2bb26c3b88
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2024-03-05 08:25:47 +0530

    Time: 21 ms (89.31%), Space: 13.8 MB (70.34%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minimumLength(string s) {
-//         "bbbbbbbbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbccbcbcbccbbabbb"
+
         int n=s.length();
         int i=0,j=n-1;
         while(i<j){
@@ -10,8 +10,7 @@ public:
             while(j>i && s[j]==first)j--;
             // cout<<i<<" "<<j<<endl;
             // cout<<s[i]<<" "<<s[j]<<endl;
-            if(i==j || s[i]!=s[j])break;
-            // else i++,j--;
+            if(s[i]!=s[j])break;
         }
         // if(i==j)return 0;
         return j-i+1;
